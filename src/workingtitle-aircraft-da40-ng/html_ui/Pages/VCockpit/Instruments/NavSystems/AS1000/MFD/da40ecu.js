5736b7b4d2c6ba32cfe8abb91bd794081325906b
Matt Nischan <nishmaster@gmail.com>
2020-09-10 16:29:48 -0500

    Moved things into better folders and made a DA40NG project.

--- src/workingtitle-aircraft-da40-ng/html_ui/Pages/VCockpit/Instruments/NavSystems/AS1000/MFD/da40ecu.js
+++ src/workingtitle-aircraft-da40-ng/html_ui/Pages/VCockpit/Instruments/NavSystems/AS1000/MFD/da40ecu.js
@@ -56,26 +56,20 @@ class DA40ECU {
     if (!this._previousTime) {
       this._previousTime = time;
     }
+
     else {
       let iterationTime = time - this._previousTime;
 
       let currentRpm = SimVar.GetSimVarValue("PROP RPM:1", "rpm");
       let currentLeverPos = SimVar.GetSimVarValue("GENERAL ENG THROTTLE LEVER POSITION:1", "Percent");
 
-      let currentTorque = SimVar.GetSimVarValue("ENG TORQUE:1", "Foot pounds");
-      let currentEngineRpm = SimVar.GetSimVarValue("GENERAL ENG RPM:1", "rpm");
-
-      let maxHp = 180.7;
-      let currentHp = currentTorque * currentEngineRpm / 5252;
-
-      let currentLoad = Math.max(0, Math.min(100, (currentHp / maxHp) * 100));
       let targetRpm = this.getTargetRpm(currentLeverPos);
 
       let error = currentRpm - targetRpm;
       let integral = this._previousRpmIntegral + error * iterationTime;
       let derivative = (error - this._previousRpmError) / iterationTime;
 
-      let output = (.005 * error) + (0 * integral) + (0 * derivative);
+      let output = (.005 * error) + (0 * integral) + (0 * derivative); //Seems stable with no Ki or Kd for now
 
       this._previousRpmError = error;
       this._previousRpmIntegral = integral;
@@ -84,29 +78,22 @@ class DA40ECU {
       let newProp = Math.min(100, Math.max(0, currentProp - output));
 
       SimVar.SetSimVarValue("GENERAL ENG PROPELLER LEVER POSITION:1", "Percent", newProp);
-      if (currentLeverPos >= 92) {
-        //SimVar.SetSimVarValue("GENERAL ENG THROTTLE LEVER POSITION:1", "Percent", 100);
-      }
-
-      let inputStatus = GetInputStatus("PLANE", "KEY_AXIS_THROTTLE_SET");
 
       this._previousTime = time;
-
-      console.log(`RPM: ${Math.round(currentRpm)}|${this.getTargetRpm(currentLeverPos)}|${this.getTargetRpm(currentLoad)} | HP: ${currentHp.toFixed(2)}`)
-      console.log(`Current: ${currentProp.toFixed(2)} | New: ${newProp.toFixed(2)} | Thr: ${currentLeverPos.toFixed(4)}`);
     }
   }
 
   /**
-   * Gets the target RPM for a given power setting.
-   * @param {number} power The power setting, expressed as a percentage in 100s. 
+   * Gets the target RPM for a given throttle setting. Derived experimentally from the
+   * default engine model to produce the correct load/rpm ratios.
+   * @param {number} power The throttle setting, expressed as a percentage in 100s. 
    */
   getTargetRpm(power) {
     if (power >= 90.5853) {
       return 2100 + (((power - 90.5853) / 9.4147) * 200);
     }
     if (power >= 20) {
-      return 1800 + (((power - 20) / 72) * 300);
+      return 1800 + (((power - 20) / 70.5853) * 300);
     }
     else {
       return 2150 - (power / 20) * 250;
