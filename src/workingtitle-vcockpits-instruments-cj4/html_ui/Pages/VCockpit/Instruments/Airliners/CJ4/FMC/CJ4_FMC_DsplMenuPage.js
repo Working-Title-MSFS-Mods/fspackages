b3bd32f38bf65628fe5139bcea16c4d4ded6698f
dga711 <dga711@users.noreply.github.com>
2020-09-23 10:06:08 +0200

    left display menu

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DsplMenuPage.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DsplMenuPage.js
@@ -2,19 +2,19 @@ class CJ4_FMC_DsplMenuPage {
     static ShowPage1(fmc) {
         fmc.clearDisplay();
         fmc.setTemplate([
-            ["LEFT DISPLAY MENU[color]blue", "1", "2"],
-            ["", "", "MAP DISPLAY[color]blue"],
+            ["", "", "LEFT DISPLAY MENU[blue]", 1, 2],
+            ["", "", "MAP DISPLAY[blue]"],
             ["NEAREST APTS", "ETA"],
             [""],
-            ["HI NAVAIDS", "SPEED"],
+            ["HI NAVAIDS[green]", "SPEED"],
             [""],
             ["LO NAVAIDS", "ALTITUDE"],
             [""],
             ["INTERS", "APTS"],
             [""],
             ["TERM WPTS", "MISS APPR"],
-            ["WINDOW[color]blue", "SIDE[color]blue"],
-            ["OFF/ON/VNAV", "L/R>"]
+            ["WINDOW[blue s-text]", "SIDE[blue]"],
+            ["OFF/[s-text]ON[green]/VNAV[s-text]", "L[green]/[white]R[s-text]>"]
         ]);
         fmc.onPrevPage = () => { CJ4_FMC_DsplMenuPage.ShowPage2(fmc); };
         fmc.onNextPage = () => { CJ4_FMC_DsplMenuPage.ShowPage2(fmc); };
@@ -23,19 +23,19 @@ class CJ4_FMC_DsplMenuPage {
     static ShowPage2(fmc) {
         fmc.clearDisplay();
         fmc.setTemplate([
-            ["LEFT DISPLAY MENU[color]blue", "2", "2"],
-            ["", "", "MAP DISPLAY[color]blue"],
+            ["", "", "LEFT DISPLAY MENU[blue]", 2, 2],
+            ["", "", "MAP DISPLAY[blue]"],
             ["NDBS"],
             [""],
-            ["RNG: ALT SEL"],
+            ["RNG: ALT SEL[green]"],
             [""],
-            ["LRN POS"],
+            ["GNSS POS[green]"],
             [""],
             ["ALTN FPLN"],
             [""],
             [""],
-            ["", "SIDE[color]blue"],
-            ["", "L/R>"]
+            ["", "SIDE[blue]"],
+            ["", "L[green]/[white]R[s-text]>"]
         ]);
         fmc.onPrevPage = () => { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
         fmc.onNextPage = () => { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
