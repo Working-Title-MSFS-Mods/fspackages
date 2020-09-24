5f0eb471d63c12cadda6980545fb70e658db166c
Rob Jones <rcj@rcj.io>
2020-09-24 18:10:09 -0500

    s/Cfg/Ini/ and add jsdoc

--- src/workingtitle-vcockpits-instruments-navsystems/html_ui/Pages/VCockpit/Instruments/NavSystems/Shared/ConfigHandler.js
+++ src/workingtitle-vcockpits-instruments-navsystems/html_ui/Pages/VCockpit/Instruments/NavSystems/Shared/ConfigHandler.js
@@ -1,20 +1,36 @@
+/** class ConfigLoader loads game data in several text formats. */
 class ConfigLoader {
+    /**
+     * Creates an instance of ConfigLoader
+     * @constructor
+     * @param {string} basepath The instrument's _xmlConfigPath 
+     */
     constructor(basepath) {
         let vfspath = "/VFS/" + basepath.replace(/\\/g, "/");
         let vfspathParts = vfspath.split("/");
         this._vfspath = vfspathParts.slice(0, -2).join("/")
     }
 
+    /**
+     * Parses an INI-style configuration file
+     * @param {string} filename Path to the file relative to aircraft's top-level directory
+     * @returns {Promise} A map containing the sections and k/v pairs of the ini file
+     */
     loadIni(filename) {
         return new Promise((resolve) => {
             Utils.loadFile(`${this._vfspath}/${filename}`, (text) => {
-                let parser = new CfgParser();
+                let parser = new IniParser();
                 let out = parser.parse(text);
                 resolve(out);
             });
         });
     }
 
+    /**
+     * Parses a basic XML file
+     * @param {string} filename Path to the file relative to aircraft's top-level directory
+     * @returns {Promise} An XMLDocument representation of the file's contents
+     */
     loadXml(filename) {
         return new Promise((resolve) => {
             Utils.loadFile(`${this._vfspath}/${filename}`, (text) => {
@@ -25,10 +41,15 @@ class ConfigLoader {
         });
     }
 
-    // The model xml files aren't properly formed because they have multiple root
-    // nodes. (Seriously?)   We'll wrap them in a fake root node so that we can
-    // parse them properly.  Hopefully that's actually a consistent format.
+    /**
+     * Parses an XML file with multiple root nodes
+     * @param {string} filename Path to the file relative to aircraft's top-level directory
+     * @returns {Promise} An XMLDocument representing the file's contents wrapped in a FAKEROOT tag
+     */
     loadFrenchXml(filename) {
+        // The model xml files aren't properly formed because they have multiple root
+        // nodes. (Seriously?)   We'll wrap them in a fake root node so that we can
+        // parse them properly.  Hopefully that's actually a consistent format.
         return new Promise((resolve) => {
             Utils.loadFile(`${this._vfspath}/${filename}`, (text) => {
                 // This ugly regex is to remove the XML DTD
@@ -41,6 +62,11 @@ class ConfigLoader {
         });
     }
 
+    /**
+     * Opens and returns the text of any arbitrary file
+     * @param {string} filename Path to the file relative to aircraft's top-level directory
+     * @returns {Promise} The contents of the file as a string
+     */
     loadRawFile(filename) {
         return new Promise((resolve) => {
             Utils.loadFile(`${this._vfspath}/${filename}`, (text) => {
@@ -49,6 +75,11 @@ class ConfigLoader {
         });        
     }
 
+    /**
+     * Finds and returns an XML model definition
+     * @param {string} where Whether to load the 'interior' or 'exterior' model 
+     * @returns {Promise} An XMLDocument representing the model's definition wrapped in a FAKEROOT tag
+     */
     loadModelFile(where) {
         return new Promise((resolve, reject) => {
             this.loadIni("model/model.cfg")
@@ -64,26 +95,38 @@ class ConfigLoader {
         });
     }
 
-
-    loadExteriorModel(where) {
+    /**
+     * Convenience function to get a plane's exterior model definition
+     * @returns {Promise}  An XMLDocument representing the model's definition wrapped in a FAKEROOT tag
+     */
+    loadExteriorModel() {
         return this.loadModelFile("exterior");
     }
-
-    loadInteriorModel(where) {
+    /**
+     * Convenience function to get a plane's interior model definition
+     * @returns {Promise}  An XMLDocument representing the model's definition wrapped in a FAKEROOT tag
+     */
+    loadInteriorModel() {
         return this.loadModelFile("interior")
     }
 }
 
 
-class CfgParser {
-    parse(cfgString) {
+/** class IniParser processes an ini-style configuration text */
+class IniParser {
+    /**
+     * Parses an ini-style text block into a map
+     * @param {string} iniString The ini content as text 
+     * @returns {Map} The sections and k/v pairs of the ini file
+     */
+    parse(iniString) {
         var out = {};
 
         var ptr = out;
         var section = null;
 
         var re = /^\[([^\]]*)\]$|^([^=\s]+)\s*= *([^#\s]+)/i;
-        var lines = cfgString.split(/[\r\n]+/g);
+        var lines = iniString.split(/[\r\n]+/g);
 
         lines.forEach((line, crap, morecrap) => {
             // skip blank likes and comments
