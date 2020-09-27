aac4f91d24c80e4a9702118958af86c2b608b8c9
dga711 <dga711@users.noreply.github.com>
2020-09-27 02:02:48 +0200

    ocd on the dsplmenupage

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DsplMenuPage.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DsplMenuPage.js
@@ -1,103 +1,75 @@
 class CJ4_FMC_DsplMenuPage {
+
+    static toggleSymbol(_symbol) {
+        return new Promise(function (resolve, reject) {
+            let symbols = SimVar.GetSimVarValue("L:CJ4_MAP_SYMBOLS", "number");
+            if (symbols == -1)
+                resolve(); // if it fails, it fails
+            symbols ^= (1 << _symbol);
+            SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "number", symbols).then(() => {
+                resolve();
+            });
+        });
+    }
+
+    static hasSymbol(_symbol) {
+        let symbols = SimVar.GetSimVarValue("L:CJ4_MAP_SYMBOLS", "number");
+        if (symbols == -1)
+            return 0;
+        if (symbols & (1 << _symbol))
+            return 1;
+        return 0;
+    }
+
     static ShowPage1(fmc) {
         fmc.clearDisplay();
-		
-		let loNavaidsActive = fmc.DSPLMENUloNavaids == 0 ? "LO NAVAIDS[white][s-text]"
-            : "LO NAVAIDS[green]";
-		
-		let intersectionsActive = fmc.DSPLMENUintersections == 0 ? "INTERS[white][s-text]"
-            : "INTERS[green]";
-			
-		let airportsActive = fmc.DSPLMENUairports == 0 ? "APTS[white][s-text]"
-            : "APTS[green]";	
-		
-		let altitudeActive = fmc.DSPLMENUaltitude == 0 ? "ALTITUDE[white][s-text]"
-            : "ALTITUDE[green]";
-			
-		
+
+        // get map symbols and render template
+        let loNavaidsActive = fmc._templateRenderer.renderSwitch(["LO NAVAIDS"], (this.hasSymbol(CJ4_MapSymbol.NAVAIDS) - 1));
+        let intersectionsActive = fmc._templateRenderer.renderSwitch(["INTERS"], (this.hasSymbol(CJ4_MapSymbol.INTERSECTS) - 1));
+        let airportsActive = fmc._templateRenderer.renderSwitch(["APTS"], (this.hasSymbol(CJ4_MapSymbol.AIRPORTS) - 1));
+        let altitudeActive = fmc._templateRenderer.renderSwitch(["ALTITUDE"], (this.hasSymbol(CJ4_MapSymbol.CONSTRAINTS) - 1));
+
+        fmc.onLeftInput[2] = () => {
+            this.toggleSymbol(CJ4_MapSymbol.NAVAIDS).then(() => {
+                CJ4_FMC_DsplMenuPage.ShowPage1(fmc);
+            });
+        }
+
+        fmc.onLeftInput[3] = () => {
+            this.toggleSymbol(CJ4_MapSymbol.INTERSECTS).then(() => {
+                CJ4_FMC_DsplMenuPage.ShowPage1(fmc);
+            });
+        }
+
+        fmc.onRightInput[2] = () => {
+            this.toggleSymbol(CJ4_MapSymbol.CONSTRAINTS).then(() => {
+                CJ4_FMC_DsplMenuPage.ShowPage1(fmc);
+            });
+        }
+
+        fmc.onRightInput[3] = () => {
+            this.toggleSymbol(CJ4_MapSymbol.AIRPORTS).then(() => {
+                CJ4_FMC_DsplMenuPage.ShowPage1(fmc);
+            });
+        }
+
         fmc._templateRenderer.setTemplateRaw([
             [" LEFT DISPLAY MENU[blue]", "1/2 [blue]"],
             ["", "", "MFD MAP DISPLAY[blue]"],
             ["NEAREST APTS[s-text]", "ETA[s-text]"],
             [""],
-            ["HI NAVAIDS[s-text]", "SPEED"],
+            ["HI NAVAIDS[s-text]", "SPEED[s-text]"],
             [""],
             [loNavaidsActive, altitudeActive],
             [""],
             [intersectionsActive, airportsActive],
             [""],
-            ["TERM WPTS", "MISS APPR"],
+            ["TERM WPTS[s-text]", "MISS APPR[s-text]"],
             ["WINDOW[blue s-text]", "SIDE[blue]"],
             ["OFF/[s-text]ON[green]/VNAV[s-text]", "L[green]/[white]R[s-text]>"]
         ]);
-		fmc.onLeftInput[2] = () => {
-            if (fmc.DSPLMENUloNavaids == 0) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter + 64;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-                fmc.DSPLMENUloNavaids = 1;
-            } else if (fmc.DSPLMENUloNavaids == 1) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter - 64;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-				fmc.DSPLMENUloNavaids = 0;
-            }
-            loNavaidsActive = fmc.DSPLMENUloNavaids == 0 ? "LO NAVAIDS[white][s-text]"
-            : "LO NAVAIDS[green]";
-				
-            fmc.clearUserInput();
-            { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
-        }
-		
-		fmc.onLeftInput[3] = () => {
-            if (fmc.DSPLMENUintersections == 0) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter + 32;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-                fmc.DSPLMENUintersections = 1;
-            } else if (fmc.DSPLMENUintersections == 1) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter - 32;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-				fmc.DSPLMENUintersections = 0;
-            }
-            intersectionsActive = fmc.DSPLMENUintersections == 0 ? "INTERS[white][s-text]"
-            : "INTERS[green]";
-				
-            fmc.clearUserInput();
-            { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
-        }
-		
-		fmc.onRightInput[2] = () => {
-            if (fmc.DSPLMENUaltitude == 0) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter + 2;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-                fmc.DSPLMENUaltitude = 1;
-            } else if (fmc.DSPLMENUaltitude == 1) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter - 2;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-				fmc.DSPLMENUaltitude = 0;
-            }
-            altitudeActive = fmc.DSPLMENUaltitude == 0 ? "ALTITUDE[white][s-text]"
-            : "ALTITUDE[green]";
-				
-            fmc.clearUserInput();
-            { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
-        }
-		
-		fmc.onRightInput[3] = () => {
-            if (fmc.DSPLMENUairports == 0) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter + 16;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-                fmc.DSPLMENUairports = 1;
-            } else if (fmc.DSPLMENUairports == 1) {
-				fmc.DSPLMENUcounter = fmc.DSPLMENUcounter - 16;
-				SimVar.SetSimVarValue("L:CJ4_MAP_SYMBOLS", "Number", fmc.DSPLMENUcounter);
-				fmc.DSPLMENUairports = 0;
-            }
-            airportsActive = fmc.DSPLMENUairports == 0 ? "APTS[white][s-text]"
-            : "APTS[green]";
-				
-            fmc.clearUserInput();
-            { CJ4_FMC_DsplMenuPage.ShowPage1(fmc); };
-        }
-		
+
         fmc.onPrevPage = () => { CJ4_FMC_DsplMenuPage.ShowPage2(fmc); };
         fmc.onNextPage = () => { CJ4_FMC_DsplMenuPage.ShowPage2(fmc); };
         fmc.updateSideButtonActiveStatus();
@@ -124,4 +96,17 @@ class CJ4_FMC_DsplMenuPage {
         fmc.updateSideButtonActiveStatus();
     }
 }
+
+// don't love this code copy here, but you gotta do what you gotta do
+var CJ4_MapSymbol;
+(function (CJ4_MapSymbol) {
+    CJ4_MapSymbol[CJ4_MapSymbol["TRAFFIC"] = 0] = "TRAFFIC";
+    CJ4_MapSymbol[CJ4_MapSymbol["CONSTRAINTS"] = 1] = "CONSTRAINTS";
+    CJ4_MapSymbol[CJ4_MapSymbol["AIRSPACES"] = 2] = "AIRSPACES";
+    CJ4_MapSymbol[CJ4_MapSymbol["AIRWAYS"] = 3] = "AIRWAYS";
+    CJ4_MapSymbol[CJ4_MapSymbol["AIRPORTS"] = 4] = "AIRPORTS";
+    CJ4_MapSymbol[CJ4_MapSymbol["INTERSECTS"] = 5] = "INTERSECTS";
+    CJ4_MapSymbol[CJ4_MapSymbol["NAVAIDS"] = 6] = "NAVAIDS";
+})(CJ4_MapSymbol || (CJ4_MapSymbol = {}));
+
 //# sourceMappingURL=CJ4_FMC_FMCCommPage.js.map
