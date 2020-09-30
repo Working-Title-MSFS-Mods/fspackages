a6cf150a008101565a7da014df687ee4aefcf830
dga711 <dga711@users.noreply.github.com>
2020-09-30 10:15:33 +0200

    deparr: fix errors when deselecting departure

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DepArrPage.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DepArrPage.js
@@ -74,7 +74,9 @@ class CJ4_FMC_DepArrPage {
             rows[0] = ["", Avionics.Utils.formatRunway(selectedRunway.designation) + "[d-text green]"];
             fmc.onRightInput[0] = () => {
                 fmc.setRunwayIndex(-1, (success) => {
-                    CJ4_FMC_DepArrPage.ShowDeparturePage(fmc);
+                    fmc.setDepartureIndex(-1, () => {
+                        CJ4_FMC_DepArrPage.ShowDeparturePage(fmc);
+                    });
                 });
             };
         }
@@ -123,8 +125,10 @@ class CJ4_FMC_DepArrPage {
         if (selectedDeparture) {
             rows[0][0] = selectedDeparture.name + "[d-text green]";
             fmc.onLeftInput[0] = () => {
-                fmc.setDepartureIndex(-1, () => {
-                    CJ4_FMC_DepArrPage.ShowDeparturePage(fmc);
+                fmc.setRunwayIndex(-1, (success) => {
+                    fmc.setDepartureIndex(-1, () => {
+                        CJ4_FMC_DepArrPage.ShowDeparturePage(fmc);
+                    });
                 });
             };
         }
