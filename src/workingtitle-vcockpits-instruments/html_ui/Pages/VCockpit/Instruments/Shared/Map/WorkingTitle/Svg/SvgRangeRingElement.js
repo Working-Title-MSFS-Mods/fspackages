724b78a80ad8595599431bf22ca0549e52ced86f
cywang90 <charleswang1990@gmail.com>
2020-10-07 17:16:30 -0400

    improved SvgRangeRingElement
    shortened some variable/constant names, added support for setting stroke color and width of the range ring

--- src/workingtitle-vcockpits-instruments/html_ui/Pages/VCockpit/Instruments/Shared/Map/WorkingTitle/Svg/SvgRangeRingElement.js
+++ src/workingtitle-vcockpits-instruments/html_ui/Pages/VCockpit/Instruments/Shared/Map/WorkingTitle/Svg/SvgRangeRingElement.js
@@ -2,6 +2,8 @@ class SvgRangeRingElement extends SvgLabeledRingElement {
 	constructor() {
 		super();
 		
+		this.rangeRingStrokeColor = SvgRangeRingElement.RANGE_RING_STROKE_COLOR_DEFAULT;
+		this.rangeRingStrokeWidth = SvgRangeRingElement.RANGE_RING_STROKE_WIDTH_DEFAULT;
 		this.labelPosAngle = SvgRangeRingElement.RANGE_DISPLAY_ANGLE_DEFAULT;
 	}
 	
@@ -11,8 +13,8 @@ class SvgRangeRingElement extends SvgLabeledRingElement {
 	
 	createRing(map) {
 		let rangeRing = super.createRing(map);
-		rangeRing.setAttribute("stroke", "white");
-		rangeRing.setAttribute("stroke-width", "2");
+		rangeRing.setAttribute("stroke", this.rangeRingStrokeColor);
+		rangeRing.setAttribute("stroke-width", this.rangeRingStrokeWidth);
 		rangeRing.setAttribute("stroke-opacity", "1");
 		return rangeRing;
 	}
@@ -34,16 +36,18 @@ class SvgRangeRingElement extends SvgLabeledRingElement {
 		super.updateLabel(map);
 	}
 }
+SvgRangeRingElement.RANGE_RING_STROKE_COLOR_DEFAULT = "white";
+SvgRangeRingElement.RANGE_RING_STROKE_WIDTH_DEFAULT = 2;
 SvgRangeRingElement.RANGE_DISPLAY_ANGLE_DEFAULT = -135;
 
 class SvgRangeLabelElement {
 	constructor() {
 		this.range;
-		this.rangeDisplayFont = SvgRangeLabelElement.RANGE_DISPLAY_FONT_DEFAULT;
-		this.rangeDisplayFontSize = SvgRangeLabelElement.RANGE_DISPLAY_FONT_SIZE_DEFAULT;
-		this.rangeDisplayUnitFontSize = SvgRangeLabelElement.RANGE_DISPLAY_UNIT_FONT_SIZE;
-		this.rangeDisplayAutoFontSize = SvgRangeLabelElement.RANGE_DISPLAY_AUTO_FONT_SIZE;
-		this.rangeDisplayFontColor = SvgRangeLabelElement.RANGE_DISPLAY_FONT_COLOR_DEFAULT;
+		this.font = SvgRangeLabelElement.FONT_DEFAULT;
+		this.valueFontSize = SvgRangeLabelElement.VALUE_FONT_SIZE_DEFAULT;
+		this.unitFontSize = SvgRangeLabelElement.UNIT_FONT_SIZE_DEFAULT;
+		this.autoFontSize = SvgRangeLabelElement.AUTO_FONT_SIZE_DEFAULT;
+		this.fontColor = SvgRangeLabelElement.FONT_COLOR_DEFAULT;
 	}
 	
 	createDraw(map) {
@@ -58,24 +62,24 @@ class SvgRangeLabelElement {
 		
 		this.rangeDisplayAutoText = document.createElementNS(Avionics.SVG.NS, "text");
 		this.rangeDisplayAutoText.textContent = "AUTO";
-		this.rangeDisplayAutoText.setAttribute("fill", this.rangeDisplayFontColor);
+		this.rangeDisplayAutoText.setAttribute("fill", this.fontColor);
 		this.rangeDisplayAutoText.setAttribute("text-anchor", "middle");
-		this.rangeDisplayAutoText.setAttribute("font-size", this.rangeDisplayAutoFontSize);
-		this.rangeDisplayAutoText.setAttribute("font-family", this.rangeDisplayFont);
+		this.rangeDisplayAutoText.setAttribute("font-size", this.autoFontSize);
+		this.rangeDisplayAutoText.setAttribute("font-family", this.font);
 		this.svgElement.appendChild(this.rangeDisplayAutoText);
 		
 		this.rangeDisplayValueText = document.createElementNS(Avionics.SVG.NS, "text");
-		this.rangeDisplayValueText.setAttribute("fill", this.rangeDisplayFontColor);
+		this.rangeDisplayValueText.setAttribute("fill", this.fontColor);
 		this.rangeDisplayValueText.setAttribute("text-anchor", "end");
-		this.rangeDisplayValueText.setAttribute("font-size", this.rangeDisplayFontSize);
-		this.rangeDisplayValueText.setAttribute("font-family", this.rangeDisplayFont);
+		this.rangeDisplayValueText.setAttribute("font-size", this.valueFontSize);
+		this.rangeDisplayValueText.setAttribute("font-family", this.font);
 		this.svgElement.appendChild(this.rangeDisplayValueText);
 		
 		this.rangeDisplayUnitText = document.createElementNS(Avionics.SVG.NS, "text");
 		this.rangeDisplayUnitText.textContent = "NM";
-		this.rangeDisplayUnitText.setAttribute("fill", this.rangeDisplayFontColor);
-		this.rangeDisplayUnitText.setAttribute("font-size", this.rangeDisplayUnitFontSize);
-		this.rangeDisplayUnitText.setAttribute("font-family", this.rangeDisplayFont);
+		this.rangeDisplayUnitText.setAttribute("fill", this.fontColor);
+		this.rangeDisplayUnitText.setAttribute("font-size", this.unitFontSize);
+		this.rangeDisplayUnitText.setAttribute("font-family", this.font);
 		this.svgElement.appendChild(this.rangeDisplayUnitText);
 		
 		return this.svgElement;
@@ -101,17 +105,17 @@ class SvgRangeLabelElement {
 			valueLength = Math.floor(Math.log10(this.range)) + 1;
 		}
 		
-		let autoFontSize = _auto ? this.rangeDisplayAutoFontSize : 0;
+		let autoFontSize = _auto ? this.autoFontSize : 0;
 		
 		let autoWidth = this.rangeDisplayAutoText.getComputedTextLength();
 		let valueWidth = this.rangeDisplayValueText.getComputedTextLength();
 		let unitWidth = this.rangeDisplayUnitText.getComputedTextLength();
-		let middleBuffer = this.rangeDisplayFontSize * 0.1;
-		let leftRightBuffer = this.rangeDisplayFontSize * 0.15;
-		let topBottomBuffer = this.rangeDisplayFontSize * 0.1;
+		let middleBuffer = this.valueFontSize * 0.1;
+		let leftRightBuffer = this.valueFontSize * 0.15;
+		let topBottomBuffer = this.valueFontSize * 0.1;
 		
 		let width = Math.max(autoWidth, valueWidth + unitWidth + middleBuffer) + 2 * leftRightBuffer;
-		let height = this.rangeDisplayFontSize + autoFontSize + 2 * topBottomBuffer;
+		let height = this.valueFontSize + autoFontSize + 2 * topBottomBuffer;
 		
 		this.svgElement.setAttribute("viewBox", "0 0 " + width + " " + height);
 		this.svgElement.setAttribute("width", width);
@@ -125,14 +129,14 @@ class SvgRangeLabelElement {
 		this.rangeDisplayAutoText.setAttribute("y", topBottomBuffer + autoFontSize * 0.9);
 		
 		this.rangeDisplayValueText.setAttribute("x", leftRightBuffer + valueWidth);
-		this.rangeDisplayValueText.setAttribute("y", height - topBottomBuffer - this.rangeDisplayFontSize * 0.1);
+		this.rangeDisplayValueText.setAttribute("y", height - topBottomBuffer - this.valueFontSize * 0.1);
 		
 		this.rangeDisplayUnitText.setAttribute("x", width - leftRightBuffer - unitWidth);
-		this.rangeDisplayUnitText.setAttribute("y", height - topBottomBuffer - this.rangeDisplayFontSize * 0.1);
+		this.rangeDisplayUnitText.setAttribute("y", height - topBottomBuffer - this.valueFontSize * 0.1);
 	}
 }
-SvgRangeLabelElement.RANGE_DISPLAY_FONT_DEFAULT = "Roboto";
-SvgRangeLabelElement.RANGE_DISPLAY_FONT_SIZE_DEFAULT = 20;
-SvgRangeLabelElement.RANGE_DISPLAY_UNIT_FONT_SIZE = 15;
-SvgRangeLabelElement.RANGE_DISPLAY_AUTO_FONT_SIZE = 15;
-SvgRangeLabelElement.RANGE_DISPLAY_FONT_COLOR_DEFAULT = "#8ed0d5";
+SvgRangeLabelElement.FONT_DEFAULT = "Roboto";
+SvgRangeLabelElement.VALUE_FONT_SIZE_DEFAULT = 20;
+SvgRangeLabelElement.UNIT_FONT_SIZE_DEFAULT = 15;
+SvgRangeLabelElement.AUTO_FONT_SIZE_DEFAULT = 15;
+SvgRangeLabelElement.FONT_COLOR_DEFAULT = "#8ed0d5";
