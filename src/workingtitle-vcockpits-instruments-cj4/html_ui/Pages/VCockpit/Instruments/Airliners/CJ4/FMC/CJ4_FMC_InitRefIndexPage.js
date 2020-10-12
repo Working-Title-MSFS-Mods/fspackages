4590a9673c1f0968679804f995190903609947ce
cwburnett <70416619+cwburnett@users.noreply.github.com>
2020-10-12 16:34:01 -0600

    draft change log and change ABOUT page

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_InitRefIndexPage.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_InitRefIndexPage.js
@@ -1184,7 +1184,7 @@ class CJ4_FMC_InitRefIndexPage {
             ["Working-Title-MSFS-Mods[white s-text]"],
             [""],
             [" VERSION[blue]"],
-            ["0.4.0[s-text white]"],
+            ["0.4.1[s-text white]"],
             [""],
             [""],
             [""],
