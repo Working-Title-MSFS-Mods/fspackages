e54f75a7937aebd22c8f1229aacd1e6b96b0fa1e
dga711 <dga711@users.noreply.github.com>
2020-10-13 16:55:59 +0200

    sfx: add engine starter/combustion/shutdown

--- src/sound/gen_dga.bat
+++ src/sound/gen_dga.bat
@@ -1,10 +1,10 @@
 MediaIDCheck.exe mediaIDs.txt FlightSim.mediaid
 "%WWISEROOT%\Authoring\x64\Release\bin\WwiseConsole.exe" generate-soundbank "D:\Development\GitHub\fspackages\src\sound\FlightSim.wproj" --platform "Windows" 
 
-xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WorkingTitle.PC.PCK" "D:\Development\GitHub\fspackages\src\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WorkingTitle.PC.PCK" /y
+xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WT.PC.PCK" "D:\Development\GitHub\fspackages\src\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WT.PC.PCK" /y
 
 :CopyFile
-xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WorkingTitle.PC.PCK" "C:\Users\Dominik\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WorkingTitle.PC.PCK" /y
+xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WT.PC.PCK" "C:\Users\Dominik\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WT.PC.PCK" /y
 echo Exit code is: %ERRORLEVEL%
 IF "%ErrorLevel%"=="4" (
     Goto :CopyFile
