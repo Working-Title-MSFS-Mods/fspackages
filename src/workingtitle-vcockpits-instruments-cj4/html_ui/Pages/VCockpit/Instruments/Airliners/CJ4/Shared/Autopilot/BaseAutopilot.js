af689cdf4340822d19089590a8dd2143815569c0
dga711 <dga711@users.noreply.github.com>
2020-11-09 03:02:26 +0100

    autopilot initial

---
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/Autopilot/BaseAutopilot.js
@@ -0,0 +1,33 @@
+class WT_BaseAutopilot {
+    constructor(fpm) {
+        this._fpm = fpm;
+    }
+
+    /**
+     * Run on first activation.
+     */
+    activate() {
+
+    }
+
+    /**
+     * Update data if needed.
+     */
+    update() {
+
+    }
+
+    /**
+     * Execute.
+     */
+    execute() {
+
+    }
+
+    /**
+     * Run when deactivated.
+     */
+    deactivate() {
+
+    }
+}
---
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/Autopilot/VNavPathAutopilot.js
@@ -0,0 +1,32 @@
+class WT_VNavPathAutopilot extends WT_BaseAutopilot {
+    /**
+     * Run on first activation.
+     */
+    activate() {
+        super.activate();
+    }
+
+    /**
+     * Update data if needed.
+     */
+    update() {
+        super.update();
+
+    }
+
+    /**
+     * Execute.
+     */
+    execute() {
+        super.execute();
+
+    }
+
+    /**
+     * Run when deactivated.
+     */
+    deactivate() {
+        super.deactivate();
+
+    }
+}
