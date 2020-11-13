3c808bee4f26243935a38ffc955159fb63fe97d8
DG <dga711@users.noreply.github.com>
2020-11-13 01:30:23 +0100

    Cj4 fix typos (#553)
    * replaced a with b

---
+++ src/workingtitle-cj4-wasm/common.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+
+#include <MSFS\MSFS.h>
+#include <MSFS\MSFS_Render.h>
+#include <MSFS\Legacy\gauges.h>
+#include <SimConnect.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "SimConnectDefs.h"
+
+#include "WtUtils.h"
+
+/// <summary>
+/// The handle to the SimConnect instance.
+/// </summary>
+HANDLE hSimConnect;
+
+#endif
--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/CJ4_Shared.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/CJ4_Shared.js
@@ -146,7 +146,7 @@ class CJ4_SystemEngines extends NavSystemElement {
                 line.setAttribute("x2", (startPosX - halfWidth + gradLength[i]).toString());
                 line.setAttribute("y2", posY.toString());
                 line.setAttribute("stroke", (i == 1) ? "red" : "#52504d");
-                line.setAttribute("stroke-width", "2");
+                line.setAttribute("stroke-width", "3");
                 n1Group.appendChild(line);
                 var line = document.createElementNS(Avionics.SVG.NS, "line");
                 line.setAttribute("x1", (startPosX + halfWidth - gradLength[i]).toString());
@@ -154,7 +154,7 @@ class CJ4_SystemEngines extends NavSystemElement {
                 line.setAttribute("x2", (startPosX + halfWidth).toString());
                 line.setAttribute("y2", posY.toString());
                 line.setAttribute("stroke", (i == 1) ? "red" : "#52504d");
-                line.setAttribute("stroke-width", "2");
+                line.setAttribute("stroke-width", "3");
                 n1Group.appendChild(line);
                 posY += gradSpacing[i];
             }
@@ -227,6 +227,32 @@ class CJ4_SystemEngines extends NavSystemElement {
             this.N1RightValue.setAttribute("text-anchor", "end");
             this.N1RightValue.setAttribute("alignment-baseline", "central");
             n1Group.appendChild(this.N1RightValue);
+
+            startPosY = 70;
+
+            // engine modes
+            this.N1ModeLeft = document.createElementNS(Avionics.SVG.NS, "text");
+            this.N1ModeLeft.textContent = "TO";
+            this.N1ModeLeft.setAttribute("x", 100);
+            this.N1ModeLeft.setAttribute("y", startPosY);
+            this.N1ModeLeft.setAttribute("fill", "#cccac8");
+            this.N1ModeLeft.setAttribute("font-size", "24");
+            this.N1ModeLeft.setAttribute("writing-mode", "tb-rl");
+            this.N1ModeLeft.setAttribute("glyph-orientation-vertical", "0");
+            this.N1ModeLeft.setAttribute("font-family", "Roboto-Bold");
+            n1Group.appendChild(this.N1ModeLeft);
+
+            this.N1ModeRight = document.createElementNS(Avionics.SVG.NS, "text");
+            this.N1ModeRight.textContent = "TO";
+            this.N1ModeRight.setAttribute("x", 180);
+            this.N1ModeRight.setAttribute("y", startPosY);
+            this.N1ModeRight.setAttribute("fill", "#cccac8");
+            this.N1ModeRight.setAttribute("font-size", "24");
+            this.N1ModeRight.setAttribute("writing-mode", "tb-rl");
+            this.N1ModeRight.setAttribute("glyph-orientation-vertical", "0");
+            this.N1ModeRight.setAttribute("font-family", "Roboto-Bold");
+            n1Group.appendChild(this.N1ModeRight);
+
         }
         {
             var ittGroup = document.createElementNS(Avionics.SVG.NS, "g");
@@ -1670,6 +1696,29 @@ class CJ4_SystemEngines extends NavSystemElement {
                 trimGroup.appendChild(text);
             }
         }
+
+        // engine modes
+        this.N1ModeLeft = document.createElementNS(Avionics.SVG.NS, "text");
+        this.N1ModeLeft.textContent = "TO";
+        this.N1ModeLeft.setAttribute("x", 100);
+        this.N1ModeLeft.setAttribute("y", startPosY);
+        this.N1ModeLeft.setAttribute("fill", "#cccac8");
+        this.N1ModeLeft.setAttribute("font-size", "24");
+        this.N1ModeLeft.setAttribute("writing-mode", "tb-rl");
+        this.N1ModeLeft.setAttribute("glyph-orientation-vertical", "0");
+        this.N1ModeLeft.setAttribute("font-family", "Roboto-Bold");
+        n1Group.appendChild(this.N1ModeLeft);
+
+        this.N1ModeRight = document.createElementNS(Avionics.SVG.NS, "text");
+        this.N1ModeRight.textContent = "TO";
+        this.N1ModeRight.setAttribute("x", 180);
+        this.N1ModeRight.setAttribute("y", startPosY);
+        this.N1ModeRight.setAttribute("fill", "#cccac8");
+        this.N1ModeRight.setAttribute("font-size", "24");
+        this.N1ModeRight.setAttribute("writing-mode", "tb-rl");
+        this.N1ModeRight.setAttribute("glyph-orientation-vertical", "0");
+        this.N1ModeRight.setAttribute("font-family", "Roboto-Bold");
+        n1Group.appendChild(this.N1ModeRight);
     }
     onUpdate(_deltaTime) {
         if (!this.root)
@@ -1688,7 +1737,30 @@ class CJ4_SystemEngines extends NavSystemElement {
     }
     updateN1() {
         {
-            let N1Eng1 = SimVar.GetSimVarValue("ENG N1 RPM:1", "percent");
+            // update thrust setting
+            let thrustSetting = "TO";
+            let modeClr = thrustSetting == 0 ? "#cccac8" : "#11d011";
+            switch (SimVar.GetSimVarValue("L:THROTTLE_MODE", "number")) {
+                case 1:
+                    thrustSetting = "CRU";
+                    break;
+                case 2:
+                    thrustSetting = "CLB";
+                    break;
+                case 3:
+                    thrustSetting = "TO";
+                    break;
+                default:
+                    break;
+            }
+
+            this.N1ModeLeft.textContent = thrustSetting;
+            this.N1ModeLeft.setAttribute("fill", modeClr);
+            this.N1ModeRight.textContent = thrustSetting;
+            this.N1ModeRight.setAttribute("fill", modeClr);
+        }
+        {
+            let N1Eng1 = SimVar.GetSimVarValue("TURB ENG CORRECTED N1:1", "percent");
             let n1_y = this.N1ToPixels(N1Eng1);
             if ((this.N1LeftZoneY1 - n1_y) > 10)
                 this.N1LeftCursor.setAttribute("d", "M" + (this.N1LeftZoneX - 1) + " " + n1_y + " l-10 0 l0 " + (this.N1LeftZoneY1 - n1_y) + " l5 0 l0 " + -(this.N1LeftZoneY1 - n1_y - 8) + " Z");
@@ -1697,7 +1769,7 @@ class CJ4_SystemEngines extends NavSystemElement {
             this.N1LeftValue.textContent = N1Eng1.toFixed(1);
         }
         {
-            let N1Eng2 = SimVar.GetSimVarValue("ENG N1 RPM:2", "percent");
+            let N1Eng2 = SimVar.GetSimVarValue("TURB ENG CORRECTED N1:2", "percent");
             let n1_y = this.N1ToPixels(N1Eng2);
             if ((this.N1LeftZoneY1 - n1_y) > 10)
                 this.N1RightCursor.setAttribute("d", "M" + (this.N1RightZoneX + 1) + " " + n1_y + " l10 0 l0 " + (this.N1RightZoneY1 - n1_y) + " l-5 0 l0 " + -(this.N1RightZoneY1 - n1_y - 8) + " Z");
@@ -1738,11 +1810,11 @@ class CJ4_SystemEngines extends NavSystemElement {
     }
     updateN2() {
         {
-            let N2Eng1 = SimVar.GetSimVarValue("ENG N2 RPM:1", "percent");
+            let N2Eng1 = SimVar.GetSimVarValue("TURB ENG CORRECTED N2:1", "percent");
             this.N2LeftValue.textContent = N2Eng1.toFixed(1);
         }
         {
-            let N2Eng2 = SimVar.GetSimVarValue("ENG N2 RPM:2", "percent");
+            let N2Eng2 = SimVar.GetSimVarValue("TURB ENG CORRECTED N2:2", "percent");
             this.N2RightValue.textContent = N2Eng2.toFixed(1);
         }
     }
@@ -2708,7 +2780,7 @@ class CJ4_MapContainer extends NavSystemElementContainer {
             case Jet_NDCompass_Display.PLAN:
                 this.zoomFactor = 4.1;
                 break;
-            case Jet_NDCompass_Display.PPOS:           
+            case Jet_NDCompass_Display.PPOS:
                 this.zoomFactor = 2.8;
                 break;
             default:
@@ -2882,7 +2954,7 @@ class CJ4_MapContainer extends NavSystemElementContainer {
         }
         else {
             this.map.instrument.setAttribute('style', 'display: none');
-        } 
+        }
     }
 }
 class CJ4_Map extends MapInstrumentElement {
