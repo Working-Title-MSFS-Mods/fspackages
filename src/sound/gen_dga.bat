7c65004abe710d1616d9dc94919c5507e8cf6ab3
dga711 <dga711@users.noreply.github.com>
2020-11-20 19:16:09 +0100

    some mixing and eng startup fixes

--- src/sound/gen_dga.bat
+++ src/sound/gen_dga.bat
@@ -4,7 +4,7 @@ MediaIDCheck.exe mediaIDs.txt FlightSim.mediaid
 xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WT.PC.PCK" "D:\Development\GitHub\fspackages\src\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WT.PC.PCK" /y
 
 :CopyFile
-xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WT.PC.PCK" "C:\Users\Dominik\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WT.PC.PCK" /y
+xcopy "D:\Development\GitHub\fspackages\src\sound\GeneratedSoundBanks\Asobo_CJ4_WT.PC.PCK" "I:\MSFS_PACKAGES\Community\workingtitle-aircraft-cj4\SimObjects\Airplanes\Asobo_CJ4\sound\Asobo_CJ4_WT.PC.PCK" /y
 echo Exit code is: %ERRORLEVEL%
 IF "%ErrorLevel%"=="4" (
     Goto :CopyFile
