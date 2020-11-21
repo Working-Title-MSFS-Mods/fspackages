241ecd620782a31b2c9792037bc5419d2d7a8cdd
cywang90 <charleswang1990@gmail.com>
2020-11-21 15:53:49 -0500

    implemented orientation display

---
+++ src/workingtitle-vcockpits-instruments/html_ui/WorkingTitle/Pages/VCockpit/Instruments/Shared/Map/View/Layers/MapViewOrientationDisplayLayer.js
@@ -0,0 +1,29 @@
+class WT_MapViewOrientationDisplayLayer extends WT_MapViewLayer {
+    constructor(displayTexts, className = WT_MapViewOrientationDisplayLayer.CLASS_DEFAULT, configName = WT_MapViewOrientationDisplayLayer.CONFIG_NAME_DEFAULT) {
+        super(className, configName);
+
+        this._displayTexts = displayTexts;
+    }
+
+    _createHTMLElement() {
+        this._displayBox = document.createElement("div");
+        this._displayBox.classList.add("orientationDisplay");
+        this._displayBox.style.position = "absolute";
+
+        return this._displayBox;
+    }
+
+    get displayBox() {
+        return this._displayBox;
+    }
+
+    get displayTexts() {
+        return this._displayTexts;
+    }
+
+    onUpdate(data) {
+        this.displayBox.innerHTML = this.displayTexts[data.model.orientation.mode];
+    }
+}
+WT_MapViewOrientationDisplayLayer.CLASS_DEFAULT = "orientationDisplayLayer";
+WT_MapViewOrientationDisplayLayer.CONFIG_NAME_DEFAULT = "orientation";
