5689b3e97266c01197339bc612ab3fc148b4281d
cywang90 <charleswang1990@gmail.com>
2020-12-21 13:55:03 -0500

    updated docs

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Pages/VCockpit/Instruments/Shared/Geo/SphericalMath.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Pages/VCockpit/Instruments/Shared/Geo/SphericalMath.js
@@ -1,26 +1,30 @@
+/**
+ * A small circle. A small circle is the set of points on the Earth's surface equidistant (as measured geodetically) from a
+ * central point.
+ */
 class WT_SmallCircle {
     /**
-     *
-     * @param {WT_GVector3} normal
-     * @param {Number} radius
+     * @param {{x:Number, y:Number, z:Number}} center - the center of the new small circle, represented as a position vector in
+     *                                                  the standard geographic cartesian reference system.
+     * @param {Number} radius - the radius of the new small circle in great-arc radians.
      */
-    constructor(normal, radius) {
-        this._normal = normal.unit();
+    constructor(center, radius) {
+        this._center = new WT_GVector3(center.x, center.y, center.z).unit(true);
         this._radius = radius;
     }
 
     /**
      * @readonly
-     * @property {WT_GVector3} normal
+     * @property {WT_GVector3} center - the center of this small circle.
      * @type {WT_GVector3}
      */
-    get normal() {
-        return this._normal.readonly();
+    get center() {
+        return this._center.readonly();
     }
 
     /**
      * @readonly
-     * @property {Number} radius
+     * @property {Number} radius - the radius of this small circle, in great-arc radians.
      * @type {Number}
      */
     get radius() {
@@ -28,22 +32,29 @@ class WT_SmallCircle {
     }
 
     /**
-     *
-     * @param {WT_SmallCircle} other
-     * @param {WT_GVector3[]} [reference]
-     * @returns {WT_GVector3[]}
+     * Calculates and returns the set of intersection points between this small circle and another one. Any two small circles can have
+     * zero, one, two, or infinite points of intersection. In the first and last cases, no results are returned. For the one- or two-
+     * solution cases, an array is returned with the solution(s) located at index 0 and index 1.
+     * @param {WT_SmallCircle} other - the other small circle to test for intersections.
+     * @param {WT_GVector3[]} [reference] - an array in which to store the results. If this argument is not supplied, a new array is
+     *                                      created and returned. If one is supplied with WT_GVector3 objects at index 0 and 1, the
+     *                                      results will be stored in those objects. Otherwise, new WT_GVector3 objects will be
+     *                                      created. If an array is supplied and there are no results to return, then the array and
+     *                                      its contents will be unchanged.
+     * @returns {WT_GVector3[]} an array of intersection points represented as position vectors in the standard geographic cartesian
+     *                          reference system. Solutions, if they exist, are stored at indexes 0 and 1.
      */
     intersection(other, reference) {
         if (!reference) {
             reference = [];
         }
 
-        let normal1 = this.normal;
-        let normal2 = other.normal;
+        let center1 = this.center;
+        let center2 = other.center;
         let radius1 = this.radius;
         let radius2 = other.radius;
 
-        let dot = normal1.dot(normal2);
+        let dot = center1.dot(center2);
         if (dot === 1) {
             return reference;
         }
@@ -51,13 +62,13 @@ class WT_SmallCircle {
 
         let a = (Math.cos(radius1) - dot * Math.cos(radius2)) / (1 - dotSquared);
         let b = (Math.cos(radius2) - dot * Math.cos(radius1)) / (1 - dotSquared);
-        let intersection = WT_SmallCircle._tempVector1.set(normal1).scale(a, true).add(WT_SmallCircle._tempVector2.set(normal2).scale(b, true));
+        let intersection = WT_SmallCircle._tempVector1.set(center1).scale(a, true).add(WT_SmallCircle._tempVector2.set(center2).scale(b, true));
 
         if (intersection.length > 1) {
             return reference;
         }
 
-        let cross = WT_SmallCircle._tempVector2.set(normal1).cross(normal2, true);
+        let cross = WT_SmallCircle._tempVector2.set(center1).cross(center2, true);
         if (cross.length === 0) {
             return reference;
         }
@@ -77,10 +88,17 @@ class WT_SmallCircle {
     }
 
     /**
-     *
-     * @param {WT_SmallCircle} other
-     * @param {WT_GeoPoint[]} [reference]
-     * @returns {WT_GeoPoint[]}
+     * Calculates and returns the set of intersection points between this small circle and another one. Any two small circles can have
+     * zero, one, two, or infinite points of intersection. In the first and last cases, no results are returned. For the one- or two-
+     * solution cases, an array is returned with the solution(s) located at index 0 and index 1.
+     * @param {WT_SmallCircle} other - the other small circle to test for intersections.
+     * @param {GeoPoint[]} [reference] - an array in which to store the results. If this argument is not supplied, a new array is
+     *                                   created and returned. If one is supplied with WT_GeoPoint objects at index 0 and 1, the
+     *                                   results will be stored in those objects. Otherwise, new WT_GeoPoint objects will be
+     *                                   created. If an array is supplied and there are no results to return, then the array and
+     *                                   its contents will be unchanged.
+     * @returns {GeoPoint[]} an array of intersection points represented as lat/long coordinates. Solutions, if they exist, are
+     *                       stored at indexes 0 and 1.
      */
     intersectionGeoPoint(other, reference) {
         if (!reference) {
@@ -105,10 +123,10 @@ class WT_SmallCircle {
     }
 
     /**
-     *
-     * @param {WT_GeoPoint} point
-     * @param {Number} radius
-     * @returns {WT_SmallCircle}
+     * Creates a new small circle from a lat/long coordinate pair and radius.
+     * @param {WT_GeoPoint} point - the center of the new small circle.
+     * @param {Number} radius - the radius of the new small circle, in great-arc radians.
+     * @returns {WT_SmallCircle} a small circle.
      */
     static createFromPoint(point, radius) {
         return new WT_SmallCircle(point.cartesian(WT_SmallCircle._tempVector1), radius);
@@ -117,17 +135,38 @@ class WT_SmallCircle {
 WT_SmallCircle._tempVector1 = new WT_GVector3(0, 0, 0);
 WT_SmallCircle._tempVector2 = new WT_GVector3(0, 0, 0);
 WT_SmallCircle._tempArray = [new WT_GVector3(0, 0, 0), new WT_GVector3(0, 0, 0)];
+WT_SmallCircle._tempGeoPoint = new WT_GeoPoint(0, 0);
 
+/**
+ * A great circle. A great circle is the set of points on the Earth's surface defined by the intersection of the surface sphere
+ * with a plane that passes through the center of the Earth. A great circle is a special case of a small circle with radius exactly
+ * equal to pi / 2 great-arc radians (thus, great circles are the subset of small circles with the greatest possible circumference).
+ */
 class WT_GreatCircle extends WT_SmallCircle {
+    /**
+     * @param {{x:Number, y:Number, z:Number}} normal - the normal vector of the new great circle.
+     */
     constructor(normal) {
         super(normal, Math.PI / 2);
     }
 
     /**
-     *
-     * @param {WT_GeoPoint} point1
-     * @param {WT_GeoPoint} point2
-     * @returns {WT_GreatCircle}
+     * @readonly
+     * @property {WT_GVector3} normal - the normal vector of this great circle. This vector is perpendicular to the position
+     *                                  vectors of all points on the great circle.
+     * @type {WT_GVector3}
+     */
+    get normal() {
+        return this.center;
+    }
+
+    /**
+     * Creates a new great circle that contains two points. There are two possible great circles that contain any two unique points
+     * that differ only by their directionality (equivalently, the sign of their normal vectors or their winding). The order of
+     * points passed to this method and the right-hand rule determines which of the two is returned.
+     * @param {WT_GeoPoint} point1 - the first point that lies on the new great circle.
+     * @param {WT_GeoPoint} point2 - the second point that lies on the new great circle.
+     * @returns {WT_GreatCircle} a great circle.
      */
     static createFromPoints(point1, point2) {
         let normal1 = point1.cartesian(WT_SmallCircle._tempVector1);
@@ -136,10 +175,11 @@ class WT_GreatCircle extends WT_SmallCircle {
     }
 
     /**
-     *
-     * @param {WT_GeoPoint} point
-     * @param {Number} bearing
-     * @returns {WT_GreatCircle}
+     * Creates a new great circle defined by one point and a bearing offset. The new great circle will be equivalent to the path
+     * projected from the point with the specified initial bearing (forward azimuth).
+     * @param {WT_GeoPoint} point - a point that lies on the new great circle.
+     * @param {Number} bearing - the initial bearing.
+     * @returns {WT_GreatCircle} a great circle.
      */
     static createFromPointBearing(point, bearing) {
         let lat = point.lat * Avionics.Utils.DEG2RAD;
@@ -159,25 +199,39 @@ class WT_GreatCircle extends WT_SmallCircle {
     }
 }
 
+/**
+ * A rhumb line. A rhumb line is a set of points on the Earth's surface such that the bearing from any point to another, unique point
+ * in the set is equal to a constant value theta or theta - 180 degrees. Equivalently, a rhumb line is a set of points that lie along
+ * a straight line in a Mercator projection.
+ */
 class WT_RhumbLine {
+    /**
+     * @param {{x:Number, y:Number, z:Number}} vector - the vector representation of the line defining the new rhumb line on a
+     *                                                  Mercator projection where [0 N, 0 E] lies at (0, 0) and 1 projected unit
+     *                                                  equals 1 nautical mile along the equator.
+     */
     constructor(vector) {
-        this._vector = vector.copy();
+        this._vector = new WT_GVector3(vector.x, vector.y, vector.z);
     }
 
     /**
      * @readonly
-     * @property {WT_GVector3} vector
+     * @property {WT_GVector3} vector - the vector representation of this rhumb line.
      * @type {WT_GVector3}
      */
     get vector() {
-        return this._vector;
+        return this._vector.readonly();
     }
 
     /**
-     *
-     * @param {WT_RhumbLine} other
-     * @param {WT_GVector3} [reference]
-     * @returns {WT_GVector3}
+     * Calculates and returns the intersection point of this rhumb line and another. Two rhumb lines can intersect at exactly zero,
+     * one, or infinite points. The first and last cases, no results are returned.
+     * @param {WT_RhumbLine} other - the other rhumb line to test for an intersection.
+     * @param {WT_GVector3} [reference] - a WT_GVector3 object in which to store the result. If this argument is no supplied, then a
+     *                                    new WT_GVector3 object will be created. If an object is supplied and there are no results
+     *                                    to return, the object will remain unchanged.
+     * @returns {WT_GVector3} - the intersection point, if a unique one exists, as a position vector in the standard geographic
+     *                          cartesian reference system
      */
     intersection(other, reference) {
         if (!reference) {
@@ -192,10 +246,13 @@ class WT_RhumbLine {
     }
 
     /**
-     *
-     * @param {WT_RhumbLine} other
-     * @param {WT_GeoPoint} [reference]
-     * @returns {WT_GeoPoint}
+     * Calculates and returns the intersection point of this rhumb line and another. Two rhumb lines can intersect at exactly zero,
+     * one, or infinite points. The first and last cases, no results are returned.
+     * @param {WT_RhumbLine} other - the other rhumb line to test for an intersection.
+     * @param {WT_GeoPoint} [reference] - a WT_GeoPoint object in which to store the result. If this argument is no supplied, then a
+     *                                    new WT_GeoPoint object will be created. If an object is supplied and there are no results
+     *                                    to return, the object will remain unchanged.
+     * @returns {WT_GeoPoint} - the intersection point, if a unique one exists, as lat/long coordinates.
      */
     intersectionGeoPoint(other, reference) {
         let intersection = WT_RhumbLine._tempVector3.set(this.vector).cross(other.vector, true);
@@ -217,20 +274,20 @@ class WT_RhumbLine {
     }
 
     /**
-     *
-     * @param {WT_GeoPoint} point
-     * @param {Number} bearing
-     * @returns {WT_RhumbLine}
+     * Creates a new rhumb line from a point and bearing.
+     * @param {WT_GeoPoint} point - a point that lies on the new rhumb line.
+     * @param {Number} bearing - one of the two constant bearings of the rhumb line.
+     * @returns {WT_RhumbLine} a rhumb line.
      */
     static createFromPointBearing(point, bearing) {
         return WT_RhumbLine.createFromPoints(point, WT_RhumbLine._tempGeoPoint.set(point).offsetRhumb(bearing, WT_Unit.NMILE.getConversionFactor(WT_Unit.GA_RADIAN)));
     }
 
     /**
-     *
-     * @param {WT_GeoPoint} point1
-     * @param {WT_GeoPoint} point2
-     * @returns {WT_RhumbLine}
+     * Creates a new rhumb line that contains two points.
+     * @param {WT_GeoPoint} point1 - the first point that lies on the new rhumb line.
+     * @param {WT_GeoPoint} point2 - the second point that lies on the new rhumb line.
+     * @returns {WT_RhumbLine} a rhumb line.
      */
     static createFromPoints(point1, point2) {
         WT_RhumbLine._tempArray[1] = point1.lat;
