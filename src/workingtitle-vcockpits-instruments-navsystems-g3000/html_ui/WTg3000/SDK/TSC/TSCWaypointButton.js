02d82fb1e46804c97d784d5ec2af9b8e26a408ca
cywang90 <charleswang1990@gmail.com>
2021-01-02 00:03:37 -0500

    added icons to TSCWaypointButton

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/TSC/TSCWaypointButton.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/TSC/TSCWaypointButton.js
@@ -2,6 +2,7 @@ class WT_TSCWaypointButton extends WT_TSCButton {
     constructor() {
         super();
 
+        this._iconSrcFactory = null;
         this._waypoint = null;
         this._isInit = false;
     }
@@ -40,8 +41,8 @@ class WT_TSCWaypointButton extends WT_TSCButton {
             #icon {
                 position: absolute;
                 right: 5%;
-                top: 5%
-                height: 33%;
+                top: 10%;
+                height: 40%;
                 max-width: 15%;
             }
         `;
@@ -80,12 +81,15 @@ class WT_TSCWaypointButton extends WT_TSCButton {
         this._ident.id = "ident";
         this._name = document.createElement("div");
         this._name.id = "name";
+        this._icon = document.createElement("img");
+        this._icon.id = "icon";
 
         this._empty = document.createElement("div");
         this._empty.id = "empty";
 
         this._wrapper.appendChild(this._ident);
         this._wrapper.appendChild(this._name);
+        this._wrapper.appendChild(this._icon);
         this._wrapper.appendChild(this._empty);
     }
 
@@ -125,6 +129,13 @@ class WT_TSCWaypointButton extends WT_TSCButton {
         this._isInit = true;
     }
 
+    setIconSrcFactory(factory) {
+        this._iconSrcFactory = factory;
+        if (this._isInit) {
+            this._updateWaypoint();
+        }
+    }
+
     /**
      *
      * @param {WT_Waypoint} waypoint
@@ -134,14 +145,17 @@ class WT_TSCWaypointButton extends WT_TSCButton {
 
         this._ident.innerHTML = waypoint.ident;
         this._name.innerHTML = waypoint.name;
+        this._icon.src = this._iconSrcFactory ? this._iconSrcFactory.getSrc(waypoint) : "";
 
         this._ident.style.display = "block";
         this._name.style.display = "block";
+        this._icon.style.display = "block";
     }
 
     _showEmptyText() {
         this._ident.style.display = "none";
         this._name.style.display = "none";
+        this._icon.style.display = "none";
         this._empty.style.display = "block";
     }
 
@@ -170,3 +184,86 @@ class WT_TSCWaypointButton extends WT_TSCButton {
 }
 
 customElements.define("tsc-button-waypoint", WT_TSCWaypointButton);
+
+class WT_TSCWaypointButtonIconSrcFactory {
+    constructor(imageDir) {
+        this._imageDir = imageDir;
+    }
+
+    /**
+     *
+     * @param {WT_Airport} airport
+     * @returns {String}
+     */
+    _getAirportSrc(airport) {
+        let fuel = airport.fuel !== "";
+        switch(airport.class) {
+            case WT_Airport.Class.PAVED_SURFACE:
+                if (airport.isTowered) {
+                    return fuel ?
+                        `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_PAVED_TOWERED_SERVICED.svg` :
+                        `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_PAVED_TOWERED_NONSERVICED.svg`;
+                } else {
+                    return fuel ?
+                        `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_PAVED_NONTOWERED_SERVICED.svg` :
+                        `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_PAVED_NONTOWERED_NONSERVICED.svg`;
+                }
+            case WT_Airport.Class.SOFT_SURFACE:
+                return fuel ?
+                    `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_SOFT_SERVICED.svg` :
+                    `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_SOFT_NONSERVICED.svg`;
+            //case WT_Airport.Class.SEAPLANE:
+            //    return airport.isTowered ?
+            //        `${this._imageDir}/ICON_MAP_AIRPORT_SEAPLANE_TOWERED.svg` :
+            //        `${this._imageDir}/ICON_MAP_AIRPORT_SEAPLANE_NONTOWERED.svg`;
+            case WT_Airport.Class.HELIPORT:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_HELIPORT.svg`;
+            case WT_Airport.Class.RESTRICTED:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_RESTRICTED.svg`;
+            default:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_AIRPORT_UNKNOWN.svg`;
+        }
+    }
+
+    /**
+     *
+     * @param {WT_VOR} vor
+     * @returns {String}
+     */
+    _getVORSrc(vor) {
+        switch(vor.vorType) {
+            case WT_VOR.Type.VOR_DME:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_VOR_VORDME.svg`;
+            case WT_VOR.Type.DME:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_VOR_DME.svg`;
+            case WT_VOR.Type.VORTAC:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_VOR_VORTAC.svg`;
+            case WT_VOR.Type.TACAN:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_VOR_TACAN.svg`;
+            default:
+                return `${this._imageDir}/ICON_TSC_WAYPOINT_VOR_VOR.svg`;
+        }
+    }
+
+    /**
+     *
+     * @param {WT_Waypoint} waypoint
+     * @returns {String}
+     */
+    getSrc(waypoint) {
+        if (waypoint.icao) {
+            switch (waypoint.type) {
+                case WT_ICAOWaypoint.Type.AIRPORT:
+                    return this._getAirportSrc(waypoint);
+                case WT_ICAOWaypoint.Type.VOR:
+                    return this._getVORSrc(waypoint);
+                case WT_ICAOWaypoint.Type.NDB:
+                    return `${this._imageDir}/ICON_TSC_WAYPOINT_NDB.svg`;
+                case WT_ICAOWaypoint.Type.INT:
+                    return `${this._imageDir}/ICON_TSC_WAYPOINT_INT.svg`;
+            }
+        } else {
+            return `${this._imageDir}/ICON_TSC_WAYPOINT_USER.svg`;
+        }
+    }
+}
