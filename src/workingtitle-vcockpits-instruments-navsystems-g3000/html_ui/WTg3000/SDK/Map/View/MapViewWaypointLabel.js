cd4a594d5b8dd3876eae00bd252596f4f90031de
cywang90 <charleswang1990@gmail.com>
2021-01-03 01:05:53 -0500

    waypoint renderer now supports different factories for each context

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Map/View/MapViewWaypointLabel.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Map/View/MapViewWaypointLabel.js
@@ -48,31 +48,55 @@ class WT_MapViewWaypointLabel extends WT_MapViewSimpleTextLabel {
 
         super.update(state);
     }
-
-    /**
-     * Creates a new label for a waypoint.
-     * @param {WT_Waypoint} waypoint - the waypoint for which to create a label.
-     * @param {Number} priority - the priority for the new label.
-     * @param {Boolean} alwaysShow - whether to force the new label to be shown, even when it would otherwise culled.
-     * @returns {WT_MapViewWaypointLayer} a waypoint label.
-     */
-    static createFromWaypoint(waypoint, priority, alwaysShow) {
-        let text = waypoint.ident;
-        return new WT_MapViewWaypointLabel(waypoint, text, priority, alwaysShow);
-    }
 }
 WT_MapViewWaypointLabel.OPTIONS_DEF = {
     offset: {}
 };
 
 /**
- * A cache for waypoint labels.
+ * A factory for waypoint labels.
+ * @abstract
  */
-class WT_MapViewWaypointLabelCache {
+class WT_MapViewWaypointLabelFactory {
     /**
-     * @param {Number} size - the size of the new cache.
+     * Gets a label for a waypoint.
+     * @param {WT_Waypoint} waypoint - the waypoint for which to get a label.
+     * @param {Number} priority - the priority of the label.
+     * @param {Boolean} [alwaysShow] - whether to force the label to be shown, even when it would otherwise culled. False by default.
+     * @returns {WT_MapViewWaypointLabel} a label for the waypoint.
+     */
+    getLabel(waypoint, priority, alwaysShow = false) {
+        return null;
+    }
+}
+
+/**
+ * A factory for waypoint labels whose text value is equal to the waypoint's ident string.
+ */
+class WT_MapViewWaypointIdentLabelFactory extends WT_MapViewWaypointLabelFactory {
+    /**
+     * Gets a label for a waypoint.
+     * @param {WT_Waypoint} waypoint - the waypoint for which to get a label.
+     * @param {Number} priority - the priority of the label.
+     * @param {Boolean} [alwaysShow] - whether to force the label to be shown, even when it would otherwise culled. False by default.
+     * @returns {WT_MapViewWaypointLabel} a label for the waypoint.
+     */
+    getLabel(waypoint, priority, alwaysShow = false) {
+        return new WT_MapViewWaypointLabel(waypoint, waypoint.ident, priority, alwaysShow);
+    }
+}
+
+/**
+ * A factory for waypoint labels whose text value is equal to the waypoint's ident string. This factory caches labels for re-use after they
+ * have been created.
+ */
+class WT_MapViewWaypointIdentLabelCachedFactory extends WT_MapViewWaypointIdentLabelFactory {
+    /**
+     * @param {Number} size - the size of the new factory's cache.
      */
     constructor(size) {
+        super();
+
         this._cache = new Map();
         this._size = size;
     }
@@ -82,13 +106,13 @@ class WT_MapViewWaypointLabelCache {
      * returned.
      * @param {WT_Waypoint} waypoint - the waypoint for which to get a label.
      * @param {Number} priority - the priority for the label, if a new one has to be created.
-     * @param {Boolean} alwaysShow - whether to force the label to be shown, even when it would otherwise culled.
+     * @param {Boolean} [alwaysShow] - whether to force the label to be shown, even when it would otherwise culled. False by default.
      * @returns {WT_MapViewWaypointLabel} a label for the waypoint.
      */
     getLabel(waypoint, priority, alwaysShow = false) {
         let existing = this._cache.get(waypoint.uniqueID);
         if (!existing) {
-            existing = WT_MapViewWaypointLabel.createFromWaypoint(waypoint, priority, alwaysShow);
+            existing = super.getLabel(waypoint, priority, alwaysShow);
             this._cache.set(waypoint.uniqueID, existing);
             if (this._cache.size > this._size) {
                 this._cache.delete(this._cache.keys().next().value);
