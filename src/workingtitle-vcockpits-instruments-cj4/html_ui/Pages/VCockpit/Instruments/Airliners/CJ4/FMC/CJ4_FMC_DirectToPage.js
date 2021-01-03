875bb85c6bd251f3805feef1aff2f0d7c05175e2
Christopher Burnett <cwburnett@gmail.com>
2021-01-03 13:51:52 -0700

    Makes Direct To NRST APT page contingent on data

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DirectToPage.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC_DirectToPage.js
@@ -152,125 +152,145 @@ class CJ4_FMC_DirectToPage {
         let minRunwayValue = WTDataStore.get('WT_CJ4_MIN_NRST_RWY', 3000);
         const position = new LatLongAlt(SimVar.GetSimVarValue("GPS POSITION LAT", "degree latitude"), SimVar.GetSimVarValue("GPS POSITION LON", "degree longitude"));
 
-        let airportsFiltered = [...fmc._nearest.airports].filter(w => w.ident == "origin" || w.ident == "destination"
-            || w.longestRunwayLength >= WT_ConvertUnit.setLength(minRunwayValue));
+        if (fmc._nearest && fmc._nearest.airports.length >0) {
+            let airportsFiltered = [...fmc._nearest.airports].filter(w => w.ident == "origin" || w.ident == "destination"
+                || w.longestRunwayLength >= WT_ConvertUnit.setLength(minRunwayValue));
 
-        let airportsSorted = airportsFiltered.sort((a, b) => {
-            let aDistance = parseFloat(a.distance);
-            let bDistance = parseFloat(b.distance);
-            return aDistance - bDistance;
-        });
+            let airportsSorted = airportsFiltered.sort((a, b) => {
+                let aDistance = parseFloat(a.distance);
+                let bDistance = parseFloat(b.distance);
+                return aDistance - bDistance;
+            });
 
-        let closestAirports = [];
+            let closestAirports = [];
+            
+            const fpOrigin = fmc.flightPlanManager.getOrigin();
+            if (fpOrigin) {
+                const origin = {
+                    type: "A",
+                    ident: fpOrigin.ident,
+                    icao: fpOrigin.icao,
+                    distance: Avionics.Utils.computeGreatCircleDistance(position, fpOrigin.infos.coordinates),
+                    bearing: fmc._lnav.normalizeCourse(GeoMath.correctMagvar(Avionics.Utils.computeGreatCircleHeading(position, fpOrigin.infos.coordinates), SimVar.GetSimVarValue("MAGVAR", "degrees"))),
+                    latitude: 0,
+                    longitude: 0,
+                    longestRunway: airportsSorted.find(a => {return a.ident == "origin"}).longestRunway,
+                    longestRunwayLength: airportsSorted.find(a => {return a.ident == "origin"}).longestRunwayLength
+                }
+                closestAirports[0] = origin;
+                airportsSorted = airportsSorted.filter(w => w.icao !== fpOrigin.icao  && w.ident !== "origin");
+            }
+
+            const fpDest = fmc.flightPlanManager.getDestination();
+            if (fpDest) {
+                const destination = {
+                    type: "A",
+                    ident: fpDest.ident,
+                    icao: fpDest.icao,
+                    distance: Avionics.Utils.computeGreatCircleDistance(position, fpDest.infos.coordinates),
+                    bearing: fmc._lnav.normalizeCourse(GeoMath.correctMagvar(Avionics.Utils.computeGreatCircleHeading(position, fpDest.infos.coordinates), SimVar.GetSimVarValue("MAGVAR", "degrees"))),
+                    latitude: 0,
+                    longitude: 0,
+                    longestRunway: airportsSorted.find(a => {return a.ident == "destination"}).longestRunway,
+                    longestRunwayLength: airportsSorted.find(a => {return a.ident == "destination"}).longestRunwayLength
+                }
+                closestAirports[1] = destination;
+                airportsSorted = airportsSorted.filter(w => w.icao !== fpDest.icao && w.ident !== "destination");
+            }
+
+            const airportsNeeded = 5 - closestAirports.length;
+            for (let i = 0; i < airportsNeeded; i++) {
+                closestAirports.push(airportsSorted[i]);
+            }
+
+            closestAirports = closestAirports.sort((a, b) => {
+                let aDistance = parseFloat(a.distance);
+                let bDistance = parseFloat(b.distance);
+                return aDistance - bDistance;
+            });
         
-        const fpOrigin = fmc.flightPlanManager.getOrigin();
-        if (fpOrigin) {
-            const origin = {
-                type: "A",
-                ident: fpOrigin.ident,
-                icao: fpOrigin.icao,
-                distance: Avionics.Utils.computeGreatCircleDistance(position, fpOrigin.infos.coordinates),
-                bearing: fmc._lnav.normalizeCourse(GeoMath.correctMagvar(Avionics.Utils.computeGreatCircleHeading(position, fpOrigin.infos.coordinates), SimVar.GetSimVarValue("MAGVAR", "degrees"))),
-                latitude: 0,
-                longitude: 0,
-                longestRunway: airportsSorted.find(a => {return a.ident == "origin"}).longestRunway,
-                longestRunwayLength: airportsSorted.find(a => {return a.ident == "origin"}).longestRunwayLength
-            }
-            closestAirports[0] = origin;
-            airportsSorted = airportsSorted.filter(w => w.icao !== fpOrigin.icao  && w.ident !== "origin");
-        }
+            let waypointsCell = [];
+            let runwayCell = [];
+            let waypointsBearing = [];
 
-        const fpDest = fmc.flightPlanManager.getDestination();
-        if (fpDest) {
-            const destination = {
-                type: "A",
-                ident: fpDest.ident,
-                icao: fpDest.icao,
-                distance: Avionics.Utils.computeGreatCircleDistance(position, fpDest.infos.coordinates),
-                bearing: fmc._lnav.normalizeCourse(GeoMath.correctMagvar(Avionics.Utils.computeGreatCircleHeading(position, fpDest.infos.coordinates), SimVar.GetSimVarValue("MAGVAR", "degrees"))),
-                latitude: 0,
-                longitude: 0,
-                longestRunway: airportsSorted.find(a => {return a.ident == "destination"}).longestRunway,
-                longestRunwayLength: airportsSorted.find(a => {return a.ident == "destination"}).longestRunwayLength
-            }
-            closestAirports[1] = destination;
-            airportsSorted = airportsSorted.filter(w => w.icao !== fpDest.icao && w.ident !== "destination");
-        }
+            for (let i = 0; i < 5; i++) {
+                let airport = closestAirports[i];
+                if (airport != undefined) {
+                    const distance = parseFloat(airport.distance);
+                    const bearing = parseInt(airport.bearing);
+                    const longestRunway = airport.longestRunway;
+                    const longestRunwayLength = WT_ConvertUnit.getLength(airport.longestRunwayLength).getString(0, " ", "[white]", " ");
+                    waypointsCell[i] = "<" + airport.ident.padEnd(4, " ") + "[magenta]   " + longestRunwayLength.padStart(8, " ");
+                    waypointsBearing[i] = "" + bearing.toFixed(0).padStart(3, "0") + "°" + " /" + distance.toFixed(1);
+                    runwayCell[i] = "RW" + longestRunway + ">";
 
-        const airportsNeeded = 5 - closestAirports.length;
-        for (let i = 0; i < airportsNeeded; i++) {
-            closestAirports.push(airportsSorted[i]);
-        }
-
-        closestAirports = closestAirports.sort((a, b) => {
-            let aDistance = parseFloat(a.distance);
-            let bDistance = parseFloat(b.distance);
-            return aDistance - bDistance;
-        });
-    
-        let waypointsCell = [];
-        let runwayCell = [];
-        let waypointsBearing = [];
-
-        for (let i = 0; i < 5; i++) {
-            let airport = closestAirports[i];
-            if (airport != undefined) {
-                const distance = parseFloat(airport.distance);
-                const bearing = parseInt(airport.bearing);
-                const longestRunway = airport.longestRunway;
-                const longestRunwayLength = WT_ConvertUnit.getLength(airport.longestRunwayLength).getString(0, " ", "[white]", " ");
-                waypointsCell[i] = "<" + airport.ident.padEnd(4, " ") + "[magenta]   " + longestRunwayLength.padStart(8, " ");
-                waypointsBearing[i] = "" + bearing.toFixed(0).padStart(3, "0") + "°" + " /" + distance.toFixed(1);
-                runwayCell[i] = "RW" + longestRunway + ">";
-
-            }
-            else {
-                waypointsBearing[i] = "";
-                waypointsCell[i] = "";
-            }
-            fmc.onLeftInput[i] = () => {
-                if (waypointsCell[i] != "") {
-                    fmc.setMsg("Working...");
-                    fmc.flightPlanManager.pauseSync();
-                    let icao = closestAirports[i].icao;
-                    fmc.ensureCurrentFlightPlanIsTemporary(() => {
-                        fmc.flightPlanManager.setDestination(icao, () => {
-                            let wp = fmc.flightPlanManager.getDestination();
-                            fmc.activateDirectToWaypoint(wp, () => {
+                }
+                else {
+                    waypointsBearing[i] = "";
+                    waypointsCell[i] = "";
+                }
+                fmc.onLeftInput[i] = () => {
+                    if (waypointsCell[i] != "") {
+                        fmc.setMsg("Working...");
+                        fmc.flightPlanManager.pauseSync();
+                        let icao = closestAirports[i].icao;
+                        fmc.ensureCurrentFlightPlanIsTemporary(() => {
+                            fmc.flightPlanManager.setDestination(icao, () => {
+                                let wp = fmc.flightPlanManager.getDestination();
+                                fmc.activateDirectToWaypoint(wp, () => {
+                                    fmc.setMsg();
+                                    fmc._activatingDirectTo = true;
+                                    fmc.refreshPageCallback = () => { fmc.onLegs(); }; // TODO this seems annoying, but this is how stuff works in cj4_fmc right now
+                                    fmc.onExecDefault();
+                                });
                                 fmc.setMsg();
-                                fmc._activatingDirectTo = true;
-                                fmc.refreshPageCallback = () => { fmc.onLegs(); }; // TODO this seems annoying, but this is how stuff works in cj4_fmc right now
-                                fmc.onExecDefault();
+
+    
                             });
-                            fmc.setMsg();
-
-  
                         });
-                    });
-                };
+                    };
+                }
             }
+        
+
+            // __LSB = leftsquarebracket // __RSB = rightsquarebrackt
+            //modStr = fmc.fpHasChanged ? "MOD[white]" : "ACT[blue]";
+            fmc._templateRenderer.setTemplateRaw([
+                [" " + "NEAREST AIRPORTS[blue]"],
+                ["" + waypointsBearing[0]],
+                ["" + waypointsCell[0], runwayCell[0]],
+                ["" + waypointsBearing[1]],
+                ["" + waypointsCell[1], runwayCell[1]],
+                ["" + waypointsBearing[2]],
+                ["" + waypointsCell[2], runwayCell[2]],
+                ["" + waypointsBearing[3]],
+                ["" + waypointsCell[3], runwayCell[3]],
+                ["" + waypointsBearing[4]],
+                ["" + waypointsCell[4], runwayCell[4]],
+                ["MIN RWY   -----[s-text blue]", "UPDATE[white]"],
+                [minRunwayValue + (WT_ConvertUnit.isMetric() ? " M[white]" : " FT[white]"), "AIRPORTS>[white]"]
+            ]);
+        } else {
+            fmc._templateRenderer.setTemplateRaw([
+                [" " + "NEAREST AIRPORTS[blue]"],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                [""],
+                ["MIN RWY   -----[s-text blue]", "UPDATE[white]"],
+                [minRunwayValue + (WT_ConvertUnit.isMetric() ? " M[white]" : " FT[white]"), "AIRPORTS>[white]"]
+            ]);
         }
-
-        // __LSB = leftsquarebracket // __RSB = rightsquarebrackt
-        //modStr = fmc.fpHasChanged ? "MOD[white]" : "ACT[blue]";
-        fmc._templateRenderer.setTemplateRaw([
-            [" " + "NEAREST AIRPORTS[blue]"],
-            ["" + waypointsBearing[0]],
-            ["" + waypointsCell[0], runwayCell[0]],
-            ["" + waypointsBearing[1]],
-            ["" + waypointsCell[1], runwayCell[1]],
-            ["" + waypointsBearing[2]],
-            ["" + waypointsCell[2], runwayCell[2]],
-            ["" + waypointsBearing[3]],
-            ["" + waypointsCell[3], runwayCell[3]],
-            ["" + waypointsBearing[4]],
-            ["" + waypointsCell[4], runwayCell[4]],
-            ["MIN RWY   -----[s-text blue]", "UPDATE[white]"],
-            [minRunwayValue + (WT_ConvertUnit.isMetric() ? " M[white]" : " FT[white]"), "AIRPORTS>[white]"]
-        ]);
-
-
-
+        fmc.onRightInput[5] = () => {
+            fmc.clearUserInput();
+            CJ4_FMC_DirectToPage.ShowPage2(fmc);
+        };
         fmc.onLeftInput[5] = () => {
             let value = parseInt(fmc.inOut);
             if (value >= 0 && value < 20000) {
@@ -282,23 +302,7 @@ class CJ4_FMC_DirectToPage {
             }
             { CJ4_FMC_DirectToPage.ShowPage2(fmc); }
         };
-        fmc.onRightInput[5] = () => {
-            fmc.clearUserInput();
-            CJ4_FMC_DirectToPage.ShowPage2(fmc);
-        };
-
-        fmc.onNextPage = () => {
-            page++;
-            CJ4_FMC_DirectToPage.ShowPage(fmc, directWaypoint, page);
-        };
-        fmc.onPrevPage = () => {
-            page--;
-            page = page < 1 ? 1 : page;
-            CJ4_FMC_DirectToPage.ShowPage(fmc, directWaypoint, page);
-        };
     }
-
-
 
     /**
      * Gets available direct-to waypoints from the flight plan manager.
