f464357766ca23af9fb72fc41f26f27828ef8aee
Christopher Burnett <cwburnett@gmail.com>
2021-01-08 17:10:26 -0700

    this._activatingDirectTo missing from onexecdefault

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/FMC/CJ4_FMC.js
@@ -159,6 +159,7 @@ class CJ4_FMC extends FMCMainDisplay {
                         this.copyAirwaySelections();
                         this._isRouteActivated = false;
                         this._activatingDirectToExisting = false;
+                        this._activatingDirectTo = false;
                         this.fpHasChanged = false;
                         SimVar.SetSimVarValue("L:FMC_EXEC_ACTIVE", "number", 0);
                         if (this.refreshPageCallback) {
