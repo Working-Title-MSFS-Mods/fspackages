4532c6c083e5c285b92a60cd361b14de5b49b072
dga711 <dga711@users.noreply.github.com>
2021-01-17 03:34:17 +0100

    fix some ts things

--- src/wtsdk/src/wtsdk.ts
+++ src/wtsdk/src/wtsdk.ts
@@ -1,7 +1,7 @@
 
 import { DirectTo } from './flightplanning/DirectTo';
 import { FlightPlanManager } from './flightplanning/FlightPlanManager';
-import { FlightPlanSegment } from './flightplanning/FlightPlanSegment';
+import { FlightPlanSegment, SegmentType } from './flightplanning/FlightPlanSegment';
 import { GPS } from './flightplanning/GPS';
 import { HoldDetails, HoldTurnDirection } from './flightplanning/HoldDetails';
 import { LegsProcedure } from './flightplanning/LegsProcedure';
@@ -12,13 +12,6 @@ import { Message } from './messages/Message';
 import { MessageController } from './messages/MessageController';
 import { CJ4_FMC_MessageController } from './cj4/CJ4_FMC_MessageController';
 import { CJ4_FMC_MsgPage } from './cj4/ui/fmc/pages/CJ4_FMC_MsgPage';
+import { GeoMath } from './flightplanning/GeoMath';
 
-export { DirectTo, FlightPlanManager, FlightPlanSegment, GPS, LegsProcedure, ManagedFlightPlan, ProcedureDetails, RawDataMapper, HoldDetails, HoldTurnDirection, Message, MessageController, CJ4_FMC_MessageController, CJ4_FMC_MsgPage }
-
-// export {};
-
-// declare global {
-//   interface Window {
-//     MyTest: any;
-//   }
-// }
+export { DirectTo, GeoMath, FlightPlanManager, FlightPlanSegment, SegmentType, GPS, LegsProcedure, ManagedFlightPlan, ProcedureDetails, RawDataMapper, HoldDetails, HoldTurnDirection, Message, MessageController, CJ4_FMC_MessageController, CJ4_FMC_MsgPage }
