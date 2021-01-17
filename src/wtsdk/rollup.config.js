63216d13b97cdd01a05b2b62b7680f79c46546cb
dga711 <dga711@users.noreply.github.com>
2021-01-17 23:41:01 +0100

    glob typescript things

--- src/wtsdk/rollup.config.js
+++ src/wtsdk/rollup.config.js
@@ -4,18 +4,23 @@ const babel = require('@rollup/plugin-babel').default;
 const commonjs = require('@rollup/plugin-commonjs');
 const nodeResolve = require('@rollup/plugin-node-resolve').default;
 const replace = require('@rollup/plugin-replace');
+const multiEntry = require("rollup-plugin-multi-entry");
 
 const extensions = ['.js', '.ts'];
 const outDir = "../workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/WTLibs"
 
 module.exports = {
-    input: `${__dirname}\\src\\wtsdk.ts`,
-    treeshake:false,
+    input: {
+        include: [`${__dirname}\\src\\**\\*.ts`],
+        exclude: [`${__dirname}\\src\\types\\**\\*.d.ts`]
+    },
+    treeshake: false,
     plugins: [
         replace({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         commonjs(),
+        multiEntry(),
         babel({
             presets: ['@babel/preset-typescript', ['@babel/preset-env', {
                 targets: { browsers: ['safari 11'] },
