51c8925ae3cb8951e9470fa17a3f8018371da5dd
Matt Nischan <nishmaster@gmail.com>
2021-01-19 13:27:22 -0600

    Fix MFD crashes from missing nav preset and preselect stuff.

--- src/workingtitle-vcockpits-instruments-airliners/html_ui/Pages/VCockpit/Instruments/Airliners/Shared/WT/BaseNDCompass.js
+++ src/workingtitle-vcockpits-instruments-airliners/html_ui/Pages/VCockpit/Instruments/Airliners/Shared/WT/BaseNDCompass.js
@@ -172,8 +172,11 @@ class Jet_NDCompass extends HTMLElement {
                 }
         }
 
-        this.navPreset = new NavPresetElement(document.querySelector('#NavPreset .preset-info'));
-        this.navTransferTuning = new NavTransferTuningElement(document.querySelector('#NavPreset .preset-tuning'));
+        try {
+            this.navPreset = new NavPresetElement(document.querySelector('#NavPreset .preset-info'));
+            this.navTransferTuning = new NavTransferTuningElement(document.querySelector('#NavPreset .preset-tuning'));
+        }
+        catch (err) { }
     }
     constructArc() {
     }
@@ -426,8 +429,10 @@ class Jet_NDCompass extends HTMLElement {
         }
     }
     updateNavigationInfo() {
-        this.navPreset.update();
-
+        if (this.navPreset) {
+            this.navPreset.update();
+        }
+        
         if (this.courseGroup && this.displayMode !== Jet_NDCompass_Display.PLAN) {
             if (this.navigationMode == Jet_NDCompass_Navigation.ILS || this.navigationMode == Jet_NDCompass_Navigation.VOR || this.navigationMode == Jet_NDCompass_Navigation.NAV) {
                 const waypointName = Simplane.getNextWaypointName();
@@ -494,8 +499,11 @@ class Jet_NDCompass extends HTMLElement {
                 if (this.navigationMode == Jet_NDCompass_Navigation.ILS || this.navigationMode === Jet_NDCompass_Navigation.VOR) {
                     let beacon;
                     this.ghostNeedleGroup.setAttribute("visibility", "hidden");
-                    this.navTransferTuning.setDisplayed(false);
-                    this.navPreset.setDisplayed(true);
+
+                    if (this.navTransferTuning && this.navPreset) {
+                        this.navTransferTuning.setDisplayed(false);
+                        this.navPreset.setDisplayed(true);
+                    }
 
                     if (this.navigationMode == Jet_NDCompass_Navigation.ILS) {
                         beacon = this.gps.radioNav.getBestILSBeacon();
@@ -559,7 +567,9 @@ class Jet_NDCompass extends HTMLElement {
                         let course = SimVar.GetSimVarValue("NAV LOCALIZER:1", "degree");
 
                         let frequency = SimVar.GetSimVarValue('NAV ACTIVE FREQUENCY:1', 'MHz');
-                        this.navTransferTuning.setFrequency(frequency);
+                        if (this.navTransferTuning) {
+                            this.navTransferTuning.setFrequency(frequency);
+                        }
 
                         let deviation = (SimVar.GetSimVarValue("NAV CDI:1", "number") / 127);
                         let backCourse = SimVar.GetSimVarValue("AUTOPILOT BACKCOURSE HOLD", "bool");
@@ -572,13 +582,17 @@ class Jet_NDCompass extends HTMLElement {
                         if (navSensitivity == 1) {
                             this.ghostNeedleGroup.setAttribute("visibility", "visible");
 
-                            this.navTransferTuning.setDisplayed(true);
-                            this.navPreset.setDisplayed(false);
+                            if (this.navTransferTuning && this.navPreset) {
+                                this.navTransferTuning.setDisplayed(true);
+                                this.navPreset.setDisplayed(false);
+                            }     
                         } else {
                             this.ghostNeedleGroup.setAttribute("visibility", "hidden");
 
-                            this.navTransferTuning.setDisplayed(false);
-                            this.navPreset.setDisplayed(true);
+                            if (this.navTransferTuning && this.navPreset) {
+                                this.navTransferTuning.setDisplayed(false);
+                                this.navPreset.setDisplayed(true);
+                            }  
                         }
                     }
                     else {
@@ -586,8 +600,10 @@ class Jet_NDCompass extends HTMLElement {
                         this.setAttribute("ghost_needle_deviation", "0");
                         this.ghostNeedleGroup.setAttribute("visibility", "hidden");
 
-                        this.navTransferTuning.setDisplayed(false);
-                        this.navPreset.setDisplayed(true);
+                        if (this.navTransferTuning && this.navPreset) {
+                            this.navTransferTuning.setDisplayed(false);
+                            this.navPreset.setDisplayed(true);
+                        }
                     }
                 } 
                 this.setAttribute("display_course_deviation", displayCourseDeviation ? "True" : "False");
