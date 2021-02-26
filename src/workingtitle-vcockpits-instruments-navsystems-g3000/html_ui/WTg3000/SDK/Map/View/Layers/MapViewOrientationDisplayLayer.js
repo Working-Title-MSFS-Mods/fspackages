ef19627428e98ff7b5807cb8c67ca0e03b6ff17f
cywang90 <charleswang1990@gmail.com>
2021-02-26 18:10:06 -0500

    introduced CachedHTML class

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Map/View/Layers/MapViewOrientationDisplayLayer.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Map/View/Layers/MapViewOrientationDisplayLayer.js
@@ -52,7 +52,7 @@ class WT_MapViewOrientationDisplay extends HTMLElement {
     }
 
     connectedCallback() {
-        this._text = this.shadowRoot.querySelector(`#text`);
+        this._text = new WT_CachedHTML(this.shadowRoot.querySelector(`#text`));
     }
 
     /**
