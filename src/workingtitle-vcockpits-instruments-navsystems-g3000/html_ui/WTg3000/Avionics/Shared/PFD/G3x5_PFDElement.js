bc1db4d77b63e964bb20bca20de5571644498e3b
cywang90 <charleswang1990@gmail.com>
2021-03-03 19:51:42 -0500

    updated G3000 PFD nav/dme info display

---
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/Shared/PFD/G3x5_PFDElement.js
@@ -0,0 +1,22 @@
+class WT_G3x5_PFDElement extends NavSystemElement {
+    /**
+     * @readonly
+     * @property {WT_G3x5_PFD} instrument
+     * @type {WT_G3x5_PFD}
+     */
+    get instrument() {
+        return this.gps;
+    }
+
+    onEnter() {
+    }
+
+    onUpdate(deltaTime) {
+    }
+
+    onExit() {
+    }
+
+    onEvent(event) {
+    }
+}
