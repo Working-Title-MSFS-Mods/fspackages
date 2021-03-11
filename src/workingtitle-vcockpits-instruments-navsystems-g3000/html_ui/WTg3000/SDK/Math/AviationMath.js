519aa980b53525fe032930b6757b9ce9b3e80238
cywang90 <charleswang1990@gmail.com>
2021-03-10 19:43:31 -0500

    AviationMath now uses pressure instead of pressure alt

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Math/AviationMath.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Math/AviationMath.js
@@ -2,21 +2,21 @@ class WT_AviationMath {
     /**
      * Converts indicated airspeed (IAS) to mach.
      * @param {Number} ias - the indicated airspeed to convert, in knots.
-     * @param {Number} pressureAlt - the pressure altitude, in feet.
+     * @param {Number} pressure - the static pressure, in inches of mercury.
      * @returns {Number} the mach equivalent of the indicated airspeed.
      */
-    static iasToMach(ias, pressureAlt) {
-        return Math.pow(5 * (Math.pow((Math.pow(1 + 0.2 * Math.pow(ias / 661.4786, 2), 3.5) - 1) / Math.pow(1 - 6.8455856e-6 * pressureAlt, 5.2558797) + 1, 0.2857142857) - 1), 0.5);
+    static iasToMach(ias, pressure) {
+        return Math.pow(5 * (Math.pow((Math.pow(1 + 0.2 * Math.pow(ias / 661.4786, 2), 3.5) - 1) * 29.9212553 / pressure + 1, 0.2857142857) - 1), 0.5);
     }
 
     /**
      * Converts a mach number to indicated airspeed (IAS).
      * @param {Number} mach - the mach number to convert.
-     * @param {Number} pressureAlt - the pressure altitude, in feet.
+     * @param {Number} pressure - the static pressure, in inches of mercury.
      * @returns {Number} the indicated airspeed equivalent of the mach number, in knots.
      */
-    static machToIAS(mach, pressureAlt) {
-        return 661.4786 * Math.pow(5 * (Math.pow(1 + Math.pow(1 - 6.87558586e-6 * pressureAlt, 5.2558797) * (Math.pow(1 + Math.pow(mach, 2) / 5, 3.5) - 1), 0.2857142857) - 1), 0.5);
+    static machToIAS(mach, pressure) {
+        return 661.4786 * Math.pow(5 * (Math.pow(1 + pressure * (Math.pow(1 + Math.pow(mach, 2) / 5, 3.5) - 1) / 29.9212553, 0.2857142857) - 1), 0.5);
     }
 
     /**
@@ -36,28 +36,28 @@ class WT_AviationMath {
      * @returns {Number} the mach equivalent of the true airspeed.
      */
     static tasToMach(tas, oat) {
-        return tas / WT_AviationMath.machToTAS(1);
+        return tas / WT_AviationMath.machToTAS(1, oat);
     }
 
     /**
      * Converts indicated airspeed (IAS) to true airspeed (TAS).
      * @param {Number} ias - the indicated airspeed to convert, in knots.
-     * @param {Number} pressureAlt - the pressure altitude, in feet.
+     * @param {Number} pressure - the static pressure, in inches of mercury.
      * @param {Number} oat - the true air temperature, in degrees Celsius.
      * @returns {Number} the true airspeed equivalent of the indicated airspeed, in knots.
      */
-    static iasToTAS(ias, pressureAlt, oat) {
-        return WT_AviationMath.machToTAS(WT_AviationMath.iasToMach(ias, pressureAlt), oat);
+    static iasToTAS(ias, pressure, oat) {
+        return WT_AviationMath.machToTAS(WT_AviationMath.iasToMach(ias, pressure), oat);
     }
 
     /**
      * Converts true airspeed (TAS) to indicated airspeed (IAS).
      * @param {Number} tas - the indicated airspeed to convert, in knots.
-     * @param {Number} pressureAlt - the pressure altitude, in feet.
+     * @param {Number} pressure - the static pressure, in inches of mercury.
      * @param {Number} oat - the true air temperature, in degrees Celsius.
      * @returns {Number} the indicated airspeed equivalent of the true airspeed, in knots.
      */
-    static tasToIAS(tas, pressureAlt, oat) {
-        return WT_AviationMath.machToIAS(WT_AviationMath.tasToMach(tas, oat), pressureAlt);
+    static tasToIAS(tas, pressure, oat) {
+        return WT_AviationMath.machToIAS(WT_AviationMath.tasToMach(tas, oat), pressure);
     }
 }
