d5ba0be90bd23d05718cba2b62c16a599fdbcbc4
cywang90 <charleswang1990@gmail.com>
2021-03-20 17:51:19 -0400

    fixed PFD AoA indicator not turning off properly

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/Shared/PFD/G3x5_PFDAoAIndicator.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/Shared/PFD/G3x5_PFDAoAIndicator.js
@@ -1,13 +1,12 @@
 class WT_G3x5_PFDAoAIndicator extends WT_G3x5_PFDElement {
-    constructor(instrumentID) {
+    constructor() {
         super();
 
-        this._instrumentID = instrumentID;
         this._initController();
     }
 
     _initController() {
-        this._controller = new WT_DataStoreController(this._instrumentID, null);
+        this._controller = new WT_DataStoreController("PFD", null);
         this._controller.addSetting(this._aoaModeSetting = new WT_G3x5_PFDAoAModeSetting(this._controller));
 
         this._controller.init();
