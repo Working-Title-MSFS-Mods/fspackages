86534868236e592b9c3256b77366dd51ecad80f6
cywang90 <charleswang1990@gmail.com>
2021-03-23 19:49:28 -0400

    longitude PFD joystick now can control PFD inset map range

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G5000/PFD/Main/G5000_PFD.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G5000/PFD/Main/G5000_PFD.js
@@ -1,12 +1,43 @@
 class WT_G5000_PFD extends WT_G3x5_PFD {
     get templateID() { return "AS3000_PFD"; }
 
+    _createInsetMap() {
+        return new WT_G5000_PFDInsetMap("PFD", this.citySearcher);
+    }
+
     _createMainPage() {
         return new WT_G5000_PFDMainPage(this);
     }
 
     _createApproachNavLoader() {
         return new WT_G5000_ApproachNavLoader(this.airplane);
+    }
+}
+
+class WT_G5000_PFDInsetMap extends WT_G3x5_PFDInsetMap {
+    _changeMapRange(delta) {
+        let currentIndex = this.navMap.rangeSetting.getValue();
+        let newIndex = Math.max(Math.min(currentIndex + delta, WT_G3x5_NavMap.MAP_RANGE_LEVELS.length - 1), 0);
+        this.navMap.rangeSetting.setValue(newIndex);
+    }
+
+    _handleZoomEvent(event) {
+        switch (event) {
+            case "RANGE_DEC":
+                this._changeMapRange(-1);
+                break;
+            case "RANGE_INC":
+                this._changeMapRange(1);
+                break;
+        }
+    }
+
+    onEvent(event) {
+        if (!this._isEnabled) {
+            return;
+        }
+
+        this._handleZoomEvent(event);
     }
 }
 
