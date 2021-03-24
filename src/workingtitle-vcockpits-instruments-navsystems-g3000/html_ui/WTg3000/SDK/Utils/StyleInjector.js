8ca0dbb19794f0e522465da117793dccc18460c6
cywang90 <charleswang1990@gmail.com>
2021-03-23 22:20:45 -0400

    updated jsdoc

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/StyleInjector.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/StyleInjector.js
@@ -1,4 +1,8 @@
 class WT_StyleInjector {
+    /**
+     * Injects css style into the document.
+     * @param {String} css - a string containing css style code.
+     */
     static inject(css) {
         let style = document.createElement("style");
         style.appendChild(document.createTextNode(css));
