a7a56d963364814eb1d8e602fcf74c47041fe28d
cywang90 <charleswang1990@gmail.com>
2021-03-29 22:17:59 -0400

    refactored MFD initialization code

---
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G3000/MFD/Main/G3000_MFD.js
@@ -0,0 +1,5 @@
+class WT_G3000_MFD extends WT_G3x5_MFD {
+
+}
+
+registerInstrument("as3000-mfd-element", WT_G3000_MFD);
---
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G5000/MFD/Main/G5000_MFD.js
@@ -0,0 +1,5 @@
+class WT_G5000_MFD extends WT_G3x5_MFD {
+
+}
+
+registerInstrument("as3000-mfd-element", WT_G5000_MFD);
