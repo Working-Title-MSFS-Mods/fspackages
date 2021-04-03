d8482e18151dec37019603ac5721a53b362d5beb
Christopher Burnett <cwburnett@gmail.com>
2021-04-03 14:22:28 -0600

    tweaking intercept

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/Autopilot/WT_VnavAutopilot.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/Autopilot/WT_VnavAutopilot.js
@@ -1216,7 +1216,8 @@ class WT_VerticalAutopilot {
             values.verticalSpeed = this.verticalSpeed > 0 ? 100 * Math.floor(this.verticalSpeed / 100) : 100 * Math.ceil(this.verticalSpeed / 100);
             this._altInterceptValues = values;
             this._lastUpdateTime = undefined;
-            this._navModeSelector.engageVerticalSpeed(2, this._altInterceptValues.verticalSpeed, false);
+            this._navModeSelector.engageVerticalSpeed(2, values.verticalSpeed, false);
+            console.log("values.verticalSpeed " + values.verticalSpeed);
         }
         if (this.vsSlot != 2) {
             this.vsSlot = 2;
@@ -1224,27 +1225,31 @@ class WT_VerticalAutopilot {
         this._navModeSelector.checkVerticalSpeedActive();
         let now = performance.now();
         let dt = this._lastUpdateTime != undefined ? now - this._lastUpdateTime : 1001;
-        this._lastUpdateTime = now;
+        
+        console.log("now " + now + "this._lastUpdateTime " + this._lastUpdateTime);
         
         if (dt > 1000) {
+            this._lastUpdateTime = now;
+            console.log("dt: " + dt);
             const deltaAlt = this.indicatedAltitude - altitude;
-            let setVerticalSpeed = 0;
+            const currentSet = this.vsSlot2Value;
+            let setVerticalSpeed = Math.abs(currentSet) > 0 ? 100 * Math.floor(currentSet / 100) : 100 * Math.ceil(currentSet / 100);;
             //const max = Math.abs(this._altInterceptValues.verticalSpeed);
             //Math.max((this.verticalSpeed - 800) / 3, 100);
 
             //const correction = Math.min(Math.max((10 * Math.abs(deltaAlt)), 700), max);
             if (deltaAlt > 10) {
-                setVerticalSpeed = this.vsSlot2Value - 100;
+                setVerticalSpeed = setVerticalSpeed + 100;
             }
             else if (deltaAlt < -10) {
-                setVerticalSpeed = this.vsSlot2Value + 100;
+                setVerticalSpeed = setVerticalSpeed - 100;
             }
             else {
                 setVerticalSpeed = 0;
             }
-            setVerticalSpeed = 100 * Math.ceil(setVerticalSpeed / 100);
+            //setVerticalSpeed = 100 * Math.ceil(setVerticalSpeed / 100);
             this.vsSlot2Value = setVerticalSpeed;
-            if (Math.abs(deltaAlt) < 20 || Math.abs(setVerticalSpeed < 300)) {
+            if (Math.abs(deltaAlt) < 20 || Math.abs(setVerticalSpeed) < 300) {
                 this._altInterceptValues = false;
                 this._lastUpdateTime = undefined;
                 this._navModeSelector.pressureAltitudeTarget = altitude;
@@ -1386,7 +1391,7 @@ class WT_VerticalAutopilot {
                     this._altInterceptValues = false;
                 }
                 //const interceptMargin = Math.abs(this.verticalSpeed) / 100;
-                const interceptMargin = Math.max((this.verticalSpeed - 800) / 3, 100);
+                const interceptMargin = 100 + ((Math.abs(this.verticalSpeed) / 2) * ((Math.abs(this.verticalSpeed / 100) / 60)));
                 const altCaptureArmed = this.altCapArm(isClimb);
                 if (altCaptureArmed) {
                     switch(altCaptureArmed.mode) {
