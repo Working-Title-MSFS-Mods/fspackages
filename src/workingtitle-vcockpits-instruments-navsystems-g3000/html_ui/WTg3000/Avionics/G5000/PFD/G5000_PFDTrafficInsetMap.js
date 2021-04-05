98891c2bfb6df04c961f02e23c3266af060feb95
cywang90 <charleswang1990@gmail.com>
2021-04-04 20:14:34 -0400

    added traffic map center banner

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G5000/PFD/G5000_PFDTrafficInsetMap.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/G5000/PFD/G5000_PFDTrafficInsetMap.js
@@ -10,11 +10,16 @@ class WT_G5000_PFDTrafficInsetMap extends WT_G3x5_TrafficMap {
      * @returns {WT_G3x5_MapViewTrafficStatusLayer}
      */
     _createTrafficStatusLayer() {
-        return new WT_G3x5_MapViewCondensedTrafficStatusLayer(WT_G5000_PFDTrafficInsetMap.STATUS_OPERATING_MODE_TEXT);
+        return new WT_G3x5_MapViewCondensedTrafficStatusLayer(WT_G5000_PFDTrafficInsetMap.STATUS_OPERATING_MODE_TEXT, WT_G5000_PFDTrafficInsetMap.STATUS_CENTER_BANNER_TEXT);
     }
 }
 WT_G5000_PFDTrafficInsetMap.STATUS_OPERATING_MODE_TEXT = [
     "STANDBY",
     "TA/RA",
     "TA ONLY"
+];
+WT_G5000_PFDTrafficInsetMap.STATUS_CENTER_BANNER_TEXT = [
+    "STANDBY",
+    "",
+    ""
 ];
