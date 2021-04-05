19bcc62694db0e3693245636768662ceb2b3bcc0
cywang90 <charleswang1990@gmail.com>
2021-04-05 17:10:27 -0400

    added navigraph API, initial navigraph account linking code

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/Shared/TSC/Pages/G3x5_TSCDatabaseStatus.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/Avionics/Shared/TSC/Pages/G3x5_TSCDatabaseStatus.js
@@ -1,4 +1,312 @@
 class WT_G3x5_TSCDatabaseStatus extends WT_G3x5_TSCPageElement {
+    constructor(homePageGroup, homePageName) {
+        super(homePageGroup, homePageName);
+
+        this._initNavigraphAPI();
+    }
+
+    _initNavigraphAPI() {
+        this._navigraphAPI = new WT_NavigraphAPI(WT_NavigraphAPI.MAGIC_STRINGS_G3000);
+    }
+
+    /**
+     * @readonly
+     * @property {WT_G3x5_TSCDatabaseStatusHTMLElement} htmlElement
+     * @type {WT_G3x5_TSCDatabaseStatusHTMLElement}
+     */
+    get htmlElement() {
+        return this._htmlElement;
+    }
+
+    _createHTMLElement() {
+        return new WT_G3x5_TSCDatabaseStatusHTMLElement();
+    }
+
+    _initNavigraphRow(row) {
+        row.setContext({
+            title: "Navigraph Charts",
+            navigraph: this._navigraphAPI
+        });
+
+        row.addButtonListener(this._onNavigraphButtonPressed.bind(this));
+    }
+
+    async _initRows() {
+        await WT_Wait.awaitCallback(() => this.htmlElement.isInitialized, this);
+        this._initNavigraphRow(this.htmlElement.navigraphRow);
+    }
+
     init(root) {
+        this.container.title = WT_G3x5_TSCDatabaseStatus.TITLE;
+        this._htmlElement = this._createHTMLElement();
+        root.appendChild(this.htmlElement);
+
+        this._initRows();
+    }
+
+    async _linkNavigraphAccount() {
+        await this._navigraphAPI.linkAccount();
+    }
+
+    _onNavigraphButtonPressed(button) {
+        this._linkNavigraphAccount();
     }
 }
+WT_G3x5_TSCDatabaseStatus.TITLE = "Database Status";
+
+class WT_G3x5_TSCDatabaseStatusHTMLElement extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(this._getTemplate().content.cloneNode(true));
+
+        this._isInit = false;
+    }
+
+    _getTemplate() {
+        return WT_G3x5_TSCDatabaseStatusHTMLElement.TEMPLATE;
+    }
+
+    /**
+     * @readonly
+     * @type {Boolean}
+     */
+    get isInitialized() {
+        return this._isInit;
+    }
+
+    /**
+     * @readonly
+     * @type {WT_G3x5_TSCDatabaseStatusNavigraphRowButton}
+     */
+    get navigraphRow() {
+        return this._navigraphRow;
+    }
+
+    async _defineChildren() {
+        this._navigraphRow = await WT_CustomElementSelector.select(this.shadowRoot, `#chartsrow`);
+    }
+
+    async _connectedCallbackHelper() {
+        await this._defineChildren();
+        this._isInit = true;
+    }
+
+    connectedCallback() {
+        this._connectedCallbackHelper();
+    }
+}
+WT_G3x5_TSCDatabaseStatusHTMLElement.NAME = "wt-tsc-databasestatus";
+WT_G3x5_TSCDatabaseStatusHTMLElement.TEMPLATE = document.createElement("template");
+WT_G3x5_TSCDatabaseStatusHTMLElement.TEMPLATE.innerHTML = `
+    <style>
+        :host {
+            display: block;
+        }
+
+        #wrapper {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            color: white;
+            display: grid;
+            grid-template-columns: 100%;
+            grid-template-rows: var(--databasestatus-header-height, 15%) 1fr;
+            grid-gap: var(--databasestatus-header-table-margin, 1px) 0;
+        }
+            #header {
+                position: relative;
+                border-radius: 3px;
+                background-color: black;
+                border: 1px solid var(--wt-g3x5-bordergray);
+            }
+                #headertext {
+                    width: 100%;
+                    margin: 0.2em;
+                    text-align: center;
+                    font-size: var(--databasestatus-header-font-size, 1.2em);
+                }
+            #tablecontainer {
+                position: relative;
+                border-radius: 3px;
+                background: linear-gradient(#1f3445, black 25px);
+                background-color: black;
+                border: 3px solid var(--wt-g3x5-bordergray);
+            }
+                #table {
+                    position: absolute;
+                    left: var(--databasestatus-table-padding-left, 0.1em);
+                    top: var(--databasestatus-table-padding-top, 0.1em);
+                    width: calc(100% - var(--databasestatus-table-padding-left, 0.1em) - var(--databasestatus-table-padding-right, 0.1em));
+                    height: calc(100% - var(--databasestatus-table-padding-top, 0.1em) - var(--databasestatus-table-padding-bottom, 0.1em));
+                }
+                    #tableheader {
+                        position: absolute;
+                        left: 0%;
+                        top: 0%;
+                        width: 100%;
+                        height: var(--databasestatus-table-header-height, 15%);
+                        display: grid;
+                        grid-template-rows: 100%;
+                        grid-template-columns: var(--databasestatus-table-titlecol-width, 25%) var(--databasestatus-table-standbycol-width, 37.5%) var(--databasestatus-table-activecol-width, 37.5%);
+                    }
+                        .tableHeaderItem {
+                            text-align: center;
+                        }
+                        .tableHeaderSub {
+                            font-size: 0.85em;
+                        }
+                    #tablerows {
+                        position: absolute;
+                        left: 0%;
+                        top: var(--databasestatus-table-header-height, 15%);
+                        width: 100%;
+                        height: calc(100% - var(--databasestatus-table-header-height, 15%));
+                        --scrolllist-padding-left: 0px;
+                        --scrolllist-padding-right: 0px;
+                        --scrolllist-padding-top: 0px;
+                        --scrolllist-padding-bottom: 0px;
+                        --scrolllist-align-items: stretch;
+                    }
+                        .tableRow {
+                            height: var(--databasestatus-table-row-height, 4em);
+                            border: 3px ridge var(--wt-g3x5-bordergray);
+                        }
+    </style>
+    <div id="wrapper">
+        <div id="header">
+            <div id="headertext">Databases Ready</div>
+        </div>
+        <div id="tablecontainer">
+            <div id="table">
+                <div id="tableheader">
+                    <div id="tableheadertitle" class="tableHeaderItem">Database</div>
+                    <div id="tableheaderstandby" class="tableHeaderItem">
+                        Standby<br><span class="tableHeaderSub">(effective)</span>
+                    </div>
+                    <div id="tableheaderactive" class="tableHeaderItem">
+                        Active<br><span class="tableHeaderSub">(expires)</span>
+                    </div>
+                </div>
+                <wt-tsc-scrolllist id="tablerows">
+                    <wt-tsc-button-databasestatus-row-navigraph slot="content" id="chartsrow" class="tableRow"></wt-tsc-button-databasestatus-row-navigraph>
+                </wt-tsc-scrolllist>
+            </div>
+        </div>
+    </div>
+`;
+
+customElements.define(WT_G3x5_TSCDatabaseStatusHTMLElement.NAME, WT_G3x5_TSCDatabaseStatusHTMLElement);
+
+class WT_G3x5_TSCDatabaseStatusRowButton extends WT_TSCButton {
+    constructor() {
+        super();
+
+        this._context = null;
+        this._isInit = false;
+    }
+
+    _createWrapperStyle() {
+        return `
+            #wrapper {
+                position: absolute;
+                left: 1%;
+                top: 1%;
+                width: 99%;
+                height: 99%;
+                display: grid;
+                grid-template-rows: 100%;
+                grid-template-columns: var(--databasestatus-table-titlecol-width, 25%) var(--databasestatus-table-standbycol-width, 37.5%) var(--databasestatus-table-activecol-width, 37.5%);
+                justify-items: center;
+                align-items: center;
+            }
+        `;
+    }
+
+    _createTitleStyle() {
+        return `
+            #title {
+                font-size: 0.85em;
+            }
+        `;
+    }
+
+    _createStandbyStyle() {
+        return "";
+    }
+
+    _createActiveStyle() {
+        return "";
+    }
+
+    _createStyle() {
+        let style = super._createStyle();
+
+        let titleStyle = this._createTitleStyle();
+        let standbyStyle = this._createStandbyStyle();
+        let activeStyle = this._createActiveStyle();
+
+        return`
+            ${style}
+            ${titleStyle}
+            ${standbyStyle}
+            ${activeStyle}
+        `;
+    }
+
+    _appendChildren() {
+        this._title = document.createElement("div");
+        this._title.id = "title";
+        this._standby = document.createElement("div");
+        this._standby.id = "standby";
+        this._active = document.createElement("div");
+        this._active.id = "active";
+
+        this._wrapper.appendChild(this._title);
+        this._wrapper.appendChild(this._standby);
+        this._wrapper.appendChild(this._active);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this._isInit = true;
+
+        if (this._context) {
+            this._updateFromContext();
+        }
+    }
+
+    _clearDisplay() {
+        this._title.textContent = "";
+    }
+
+    _updateTitle() {
+        this._title.textContent = this._context.title;
+    }
+
+    _updateDisplayFromContext() {
+        this._updateTitle();
+    }
+
+    _updateFromContext() {
+        if (!this._context) {
+            this._clearDisplay();
+        } else {
+            this._updateDisplayFromContext();
+        }
+    }
+
+    setContext(context) {
+        this._context = context;
+        if (this._isInit) {
+            this._updateFromContext();
+        }
+    }
+}
+
+class WT_G3x5_TSCDatabaseStatusNavigraphRowButton extends WT_G3x5_TSCDatabaseStatusRowButton {
+}
+WT_G3x5_TSCDatabaseStatusNavigraphRowButton.NAME = "wt-tsc-button-databasestatus-row-navigraph";
+
+customElements.define(WT_G3x5_TSCDatabaseStatusNavigraphRowButton.NAME, WT_G3x5_TSCDatabaseStatusNavigraphRowButton);
---
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Charts/Navigraph.js
@@ -0,0 +1,198 @@
+class WT_NavigraphAPI {
+    constructor(magicStrings) {
+        this.RFRSH_TOKEN_KEY = "WT_NG_REFRESH_TOKEN";
+        this.ACC_TOKEN_KEY = "WT_NG_ACC_TOKEN";
+        this._refreshToken = "";
+        this._chartListCache = new Map();
+        this._chartCacheTimestamp = 0;
+        this._accessTokenTimestamp = 0;
+        this._magicStrings = magicStrings;
+    }
+    /** Gets a boolean indicating if the navigraph account is linked */
+    get isAccountLinked() {
+        this._refreshToken = WTDataStore.get(this.RFRSH_TOKEN_KEY, "");
+        return this._refreshToken !== "";
+    }
+    /** Sets the refresh token */
+    set refreshToken(val) {
+        this._refreshToken = val;
+        WTDataStore.set(this.RFRSH_TOKEN_KEY, val);
+    }
+    get refreshToken() {
+        return WTDataStore.get(this.RFRSH_TOKEN_KEY, "");
+    }
+    /** Returns a boolean indicating if a access token is known */
+    get hasAccessToken() {
+        return this.accessToken !== null;
+    }
+    /** Sets the access token */
+    set accessToken(val) {
+        localStorage.setItem(this.ACC_TOKEN_KEY, val);
+        this._accessTokenTimestamp = Date.now();
+    }
+    /** Gets the access token */
+    get accessToken() {
+        return localStorage.getItem(this.ACC_TOKEN_KEY);
+    }
+    /**
+     * Checks if the access token is still good or starts the link account process
+     */
+    validateToken() {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            if (this.isAccountLinked) {
+                if (!this.hasAccessToken ||
+                    (this.hasAccessToken && (Date.now() - this._accessTokenTimestamp) > 900000)) {
+                    yield this.refreshAccessToken();
+                }
+            }
+            else {
+                yield this.linkAccount();
+            }
+        });
+    }
+    /**
+     * Refreshes the access token using the refresh token
+     */
+    refreshAccessToken() {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            const refreshForm = new Map([
+                ["grant_type", "refresh_token"],
+                ["refresh_token", this.refreshToken],
+            ]);
+            const refreshResp = yield this.sendRequest("https://identity.api.navigraph.com/connect/token", "post", refreshForm);
+            if (refreshResp.ok) {
+                this.refreshToken = refreshResp.json().refresh_token;
+                this.accessToken = refreshResp.json().access_token;
+            }
+            return;
+        });
+    }
+    /**
+     * Gets a list of charts for the given ICAO
+     * @param icao The ICAO of the airport to get the charts from
+     */
+    getChartsList(icao) {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            this.invalidateChartCache();
+            let chartsObj;
+            if (icao === "" || icao === "----") {
+                return chartsObj;
+            }
+            if (!this._chartListCache.has(icao)) {
+                yield this.validateToken();
+                const signedUrlResp = yield this.sendRequest(`https://charts.api.navigraph.com/2/airports/${icao}/signedurls/charts.json`, "get", null, true);
+                if (signedUrlResp.ok) {
+                    const signedUrl = signedUrlResp.data;
+                    const chartsListResp = yield this.sendRequest(signedUrl, "get");
+                    if (chartsListResp.ok) {
+                        chartsObj = chartsListResp.json();
+                        this._chartListCache.set(icao, chartsObj);
+                    }
+                }
+                return chartsObj;
+            }
+            else {
+                return this._chartListCache.get(icao);
+            }
+        });
+    }
+    invalidateChartCache() {
+        if (this._chartCacheTimestamp === 0 || ((Date.now() - this._chartCacheTimestamp) > 300000)) {
+            this._chartCacheTimestamp = Date.now();
+            this._chartListCache.clear();
+        }
+    }
+    /**
+     * Executes the navigraph account linking process
+     */
+    linkAccount() {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            this.refreshToken = "";
+            this.accessToken = "";
+            const pkce = WT_OAuthPkce.getChallenge(32);
+            const authForm = new Map([
+                ["code_challenge", pkce.code_challenge],
+                ["code_challenge_method", "S256"]
+            ]);
+            // send auth request
+            const authResp = yield this.sendRequest("https://identity.api.navigraph.com/connect/deviceauthorization", "post", authForm);
+            if (authResp.ok) {
+                // send user to page
+                OpenBrowser(authResp.json().verification_uri_complete);
+                // poll for token
+                const pollForm = new Map([
+                    ["grant_type", "urn:ietf:params:oauth:grant-type:device_code"],
+                    ["device_code", authResp.json().device_code],
+                    ["scope", "openid charts offline_access"],
+                    ["code_verifier", pkce.code_verifier]
+                ]);
+                while (!this.isAccountLinked) {
+                    yield WT_Wait.wait(4000);
+                    const pollResp = yield this.sendRequest("https://identity.api.navigraph.com/connect/token", "post", pollForm);
+                    if (pollResp.ok) {
+                        this.refreshToken = pollResp.json().refresh_token;
+                        this.accessToken = pollResp.json().access_token;
+                    }
+                }
+                return true;
+            }
+            else {
+                throw ("Auth failed");
+            }
+        });
+    }
+    /** Gets the signed png url of the requested chart */
+    getChartPngUrl(chart, dayChart = true) {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            if (chart !== undefined) {
+                yield this.validateToken();
+                const url = `https://charts.api.navigraph.com/2/airports/${chart.icao_airport_identifier}/signedurls/${dayChart ? chart.file_day : chart.file_night}`;
+                const urlResp = yield this.sendRequest(url, "get", null, true);
+                return urlResp.data;
+            }
+            return "";
+        });
+    }
+    /**
+     * Used to encapsulate requests to navigraph
+     * @param path The url the request points to
+     * @param method "GET" or "POST"
+     * @param form A map of data to send in the request body
+     * @param auth A boolean indicating if the auth token should be used for this request
+     */
+    sendRequest(path, method, form = null, auth = false) {
+        return WT_Wait.awaitGenerator(this, void 0, void 0, function* () {
+            const formData = new Map();
+            formData.set(LZUTF8.decompress(WT_NavigraphAPI.FORM_KEYS[0], { inputEncoding: "StorageBinaryString" }), LZUTF8.decompress(this._magicStrings[0], { inputEncoding: "StorageBinaryString" }));
+            formData.set(LZUTF8.decompress(WT_NavigraphAPI.FORM_KEYS[1], { inputEncoding: "StorageBinaryString" }), LZUTF8.decompress(this._magicStrings[1], { inputEncoding: "StorageBinaryString" }));
+            if (form !== null) {
+                form.forEach((v, k) => {
+                    formData.set(k, v);
+                });
+            }
+            const options = {
+                ignoreCache: false,
+                headers: {
+                    Accept: 'application/json, text/javascript, text/plain',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                // default max duration for a request
+                timeout: 10000,
+            };
+            if (auth) {
+                options.headers["Authorization"] = "Bearer " + this.accessToken;
+            }
+            const response = yield WT_URLRequest.request(method, path, null, formData, options);
+            return response;
+        });
+    }
+}
+WT_NavigraphAPI.FORM_KEYS = [
+    "ㆶᩙⷎ䗶䬠耂老",
+    "ㆶᩙⷎ䗷ᬫ෉䫨耂老"
+];
+
+WT_NavigraphAPI.MAGIC_STRINGS_G3000 = [
+    "㮺୙晦\u0303耂耀",
+    "∧娒ै╃↙慐滦欶ᦪᄝ䭍㐔㦛ⶭ⒈焹ㄼ䀀耀"
+]
