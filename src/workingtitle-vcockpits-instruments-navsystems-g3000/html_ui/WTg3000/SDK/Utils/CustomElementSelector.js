68d56e83537e339c7997b850d02631bb07b3ec38
cywang90 <charleswang1990@gmail.com>
2021-04-08 09:35:44 -0400

    added charts rotation and scroll control

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/CustomElementSelector.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/CustomElementSelector.js
@@ -4,15 +4,22 @@ class WT_CustomElementSelector {
      * element, the Promise returned by this method will resolve with a value of undefined instead.
      * @param {Element} root - the root element from which to start searching for the element.
      * @param {String} query - the query selector string to use to select the element.
+     * @param {*} [enforceType] - an optional class type to enforce on the selected element.
      * @returns {Promise<Element>} - a Promise to return the selected element.
      */
-    static async select(root, query) {
+    static async select(root, query, type) {
         let element = root.querySelector(query);
         if (!element) {
             return undefined;
         }
 
         await customElements.whenDefined(element.localName);
+        if (type !== undefined) {
+            await WT_Wait.awaitCallback(() => {
+                let element = root.querySelector(query);
+                return element instanceof type;
+            });
+        }
         return root.querySelector(query);
     }
 }
