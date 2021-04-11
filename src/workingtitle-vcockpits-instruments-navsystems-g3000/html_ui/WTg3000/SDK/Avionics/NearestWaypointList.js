faf6c55a4691e61133bdf35899dd57f42c5f6c50
cywang90 <charleswang1990@gmail.com>
2021-04-11 13:15:58 -0400

    implemented nearest airport list

---
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Avionics/NearestWaypointList.js
@@ -0,0 +1,77 @@
+class WT_NearestWaypointList {
+    /**
+     * @param {WT_PlayerAirplane} airplane
+     * @param {(icaos:String[]) => Promise<WT_ICAOWaypoint[]>} waypointFactoryMethod
+     * @param {WT_ICAOSearcher} waypointSearcher
+     */
+    constructor(airplane, waypointFactoryMethod, waypointSearcher) {
+        this._airplane = airplane;
+        this._waypointFactoryMethod = waypointFactoryMethod;
+        this._waypointSearcher = waypointSearcher;
+
+        this._searchRequest = null;
+
+        this._center = new WT_GeoPoint(0, 0);
+        this._radius = WT_Unit.NMILE.createNumber(0);
+
+        /**
+         * @type {WT_SortedArray<WT_Waypoint>}
+         */
+        this._waypoints = new WT_SortedArray(this._airportArrayComparator.bind(this));
+        this._waypointsReadOnly = new WT_ReadOnlyArray(this._waypoints.array);
+
+        this._optsManager = new WT_OptionsManager(this, WT_NearestWaypointList.OPTION_DEFS);
+    }
+
+    /**
+     *
+     * @param {WT_Waypoint} a
+     * @param {WT_Waypoint} b
+     * @returns {Number}
+     */
+    _airportArrayComparator(a, b) {
+        return a.location.distance(this._center) - b.location.distance(this._center);
+    }
+
+    /**
+     * @readonly
+     * @type {WT_ReadOnlyArray<WT_ICAOWaypoint>}
+     */
+    get waypoints() {
+        return this._waypointsReadOnly;
+    }
+
+    /**
+     * @type {WT_NumberUnit}
+     */
+    get radius() {
+        return this._radius.readonly();
+    }
+
+    set radius(radius) {
+        this._radius.set(radius);
+    }
+
+    async _searchForICAOs() {
+        let center = this._airplane.navigation.position(this._center);
+        if (!this._searchRequest) {
+            this._searchRequest = this._waypointSearcher.openRequest(center, this.radius, this.searchLimit);
+        } else {
+            await this._searchRequest.setParameters(center, this.radius, this.searchLimit);
+        }
+
+        await this._searchRequest.update();
+        return this._searchRequest.results;
+    }
+
+    async update() {
+        let icaos = await this._searchForICAOs();
+        let waypoints = await this._waypointFactoryMethod(icaos);
+        this._waypoints.clear();
+        waypoints.forEach(airport => this._waypoints.insert(airport), this);
+    }
+}
+WT_NearestWaypointList.OPTION_DEFS = {
+    searchLimit: {default: 200, auto: true},
+    radius: {default: WT_Unit.NMILE.createNumber(200)}
+};
