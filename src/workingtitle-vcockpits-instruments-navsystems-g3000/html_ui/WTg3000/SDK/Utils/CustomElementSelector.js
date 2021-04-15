a53800f660a1925a4379e94020acca4f30b2b184
cywang90 <charleswang1990@gmail.com>
2021-04-15 18:44:43 -0400

    now able to select airport in GTC nrst airports page and activate drct

--- src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/CustomElementSelector.js
+++ src/workingtitle-vcockpits-instruments-navsystems-g3000/html_ui/WTg3000/SDK/Utils/CustomElementSelector.js
@@ -2,22 +2,23 @@ class WT_CustomElementSelector {
     /**
      * Waits until a custom element has been defined and then returns it. If the query selector does not match any
      * element, the Promise returned by this method will resolve with a value of undefined instead.
+     * @template {Element} T
      * @param {Element} root - the root element from which to start searching for the element.
      * @param {String} query - the query selector string to use to select the element.
-     * @param {*} [enforceType] - an optional class type to enforce on the selected element.
-     * @returns {Promise<Element>} - a Promise to return the selected element.
+     * @param {new T} [enforceType] - an optional class type to enforce on the selected element.
+     * @returns {Promise<T>} - a Promise to return the selected element.
      */
-    static async select(root, query, type) {
+    static async select(root, query, enforceType) {
         let element = root.querySelector(query);
         if (!element) {
             return undefined;
         }
 
         await customElements.whenDefined(element.localName);
-        if (type !== undefined) {
+        if (enforceType !== undefined) {
             await WT_Wait.awaitCallback(() => {
                 let element = root.querySelector(query);
-                return element instanceof type;
+                return element instanceof enforceType;
             });
         }
         return root.querySelector(query);
